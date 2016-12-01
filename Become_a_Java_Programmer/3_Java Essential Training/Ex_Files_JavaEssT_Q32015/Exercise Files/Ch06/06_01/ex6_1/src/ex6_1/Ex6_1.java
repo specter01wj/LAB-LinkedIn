3d439b8d16d0a8bdf6e26a989c94e7b7e4a92767
specter01wj <specter01wj@gmail.com>
2016-12-01 18:49:27 -0500

    12/01/2016 v1

---
+++ Become_a_Java_Programmer/3_Java Essential Training/Ex_Files_JavaEssT_Q32015/Exercise Files/Ch06/06_01/ex6_1/src/ex6_1/Ex6_1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex6_1;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Ex6_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s = null;
+        
+        System.out.println(s);        
+             
+        String wel = "Welcome";
+        char[] chars = wel.toCharArray();
+        char lastChar = chars[chars.length - 1];
+        System.out.println(lastChar);
+    }
+    
+}
