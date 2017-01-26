5fb5c481188c5a2f86867ddae2c946a31fd54531
specter01wj <specter01wj@gmail.com>
2017-01-26 00:21:39 -0500

    01/25/2017 v1

--- Front-End/HTML5 Projects/HTML5 Projects _ Creating Surveys/Ex_Files_HTML5Proj_Survey/Exercise Files/Chapter 4/04_01/scripts/custom_survey.js
+++ Front-End/HTML5 Projects/HTML5 Projects _ Creating Surveys/Ex_Files_HTML5Proj_Survey/Exercise Files/Chapter 4/04_01/scripts/custom_survey.js
@@ -12,6 +12,50 @@ var init = function() {
   for (var i=0, len = showPanelButtons.length; i < len; i++) {
     showPanelButtons[i].addEventListener( 'click', onButtonClick, false);
   }
+
+  formElement = document.getElementById("desiredCourse");
+  formElement.addEventListener("blur", desiredCourseChanged, false);
+
+  formElement = document.getElementById("desiredCourse");
+  formElement.addEventListener("change", desiredCourseChanged, false);
+
+  formElement = document.getElementById("artistLength1");
+  formElement.addEventListener("change", artistLengthChanged, false);
+
+  formElement = document.getElementById("artistLength2");
+  formElement.addEventListener("change", artistLengthChanged, false);
+
+  formElement = document.getElementById("artistLength3");
+  formElement.addEventListener("change", artistLengthChanged, false);
+
+  formElement = document.getElementById("artistLength4");
+  formElement.addEventListener("change", artistLengthChanged, false);
+
+  formElement = document.getElementById("artistLength5");
+  formElement.addEventListener("change", artistLengthChanged, false);
+
+  formElement = document.getElementById("hoursPractice");
+  formElement.addEventListener("change", hoursPracticeChanged, false);
+
+
 }
 
 window.addEventListener( 'DOMContentLoaded', init, false);
+
+function desiredCourseChanged(e) {
+  target =  e.target;
+  theValue = target.value;
+  localStorage.setItem('desiredCourse',theValue);
+}
+
+function artistLengthChanged(e) {
+  target =  e.target;
+  theValue = target.value;
+  localStorage.setItem('artistLength',theValue);
+}
+
+function hoursPracticeChanged(e) {
+  target =  e.target;
+  theValue = target.value;
+  localStorage.setItem('hoursPractice',theValue);
+}
