4fa819af7c15d684aa13e78454c9882b2128dc5f
specter01wj <specter01wj@gmail.com>
2017-03-12 23:55:57 -0400

    03/12/2017 v1

--- Front-End/React/React.js Essential Training/Ex_Files_React_EssT/Ch02/02_02/start/dist/index.js
+++ Front-End/React/React.js Essential Training/Ex_Files_React_EssT/Ch02/02_02/start/dist/index.js
@@ -1,9 +1,15 @@
 const { createElement } = React
 const { render } = ReactDOM
 
+const style = {
+  backgroundColor: 'orange',
+  color: 'white',
+  fontFamily: 'verdana'
+}
+
 const title = createElement(
 	'h1',
-	{id: 'title', className: 'header'},
+	{id: 'title', className: 'header', style: style},
 	'Hello World'
 )
 
