9710e8ee11bbaae6494bae7e265ac162cb20076d
specter01wj <specter01wj@gmail.com>
2017-03-28 23:53:49 -0400

    03/29/2017 v1

--- Front-End/React/React.js Essential Training/Ex_Files_React_EssT/Ch05/05_01/start/src/components/App.js
+++ Front-End/React/React.js Essential Training/Ex_Files_React_EssT/Ch05/05_01/start/src/components/App.js
@@ -1,10 +1,11 @@
-import { createClass } from 'react'
+import { Component } from 'react'
 import { SkiDayList } from './SkiDayList'
 import { SkiDayCount } from './SkiDayCount'
 
-export const App = createClass({
-	getInitialState() {
-		return {
+export class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
 			allSkiDays: [
 			{
 				resort: "Squaw Valley",
@@ -26,12 +27,12 @@ export const App = createClass({
 			}
 		]
 		}
-	},
+	}
 	countDays(filter) {
 		const { allSkiDays } = this.state
 		return allSkiDays.filter(
 			(day) => (filter) ? day[filter] : day).length
-	},
+	}
 	render() {
 		return (
 			<div className="app">
@@ -46,7 +47,7 @@ export const App = createClass({
 			</div>
 		)
 	}
-})
+}
 
 
 
