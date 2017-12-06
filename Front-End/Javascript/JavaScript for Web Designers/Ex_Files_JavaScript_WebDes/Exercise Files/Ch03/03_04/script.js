6dcdf00e75eb8fd54ba7e989bb7a6d60c70537b6
specter01wj <specter01wj@gmail.com>
2017-12-06 01:04:33 -0500

    12/05/2017 v1

--- Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_04/script.js
+++ Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_04/script.js
@@ -1,6 +1,7 @@
 (function() {
 "use strict";
 
-	
+	var selectedCheckbox = document.querySelector('input[name=r_method]:checked').value;
+	var selectedCheckboxAll = document.querySelectorAll('input[name=r_method]');
 	
 })();
