db767d4ad675b6bb78d93d5a5589917749e85d04
specter01wj <specter01wj@gmail.com>
2017-12-06 01:08:51 -0500

    12/05/2017 v2

--- Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_05/script.js
+++ Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_05/script.js
@@ -1,6 +1,12 @@
 (function() {
 "use strict";
 
+	document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
 
+	function estimateTotal(e) {
+		e.preventDefault();
+		
+		console.log('You submitted the form.');
+	}
 
 })();
