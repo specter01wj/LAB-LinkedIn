9346123090c127dafa9955683d7842a291959087
specter01wj <specter01wj@gmail.com>
2017-12-07 01:18:30 -0500

    12/06/2017 v2

--- Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_07/script.js
+++ Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_07/script.js
@@ -1,7 +1,27 @@
 (function() {
 "use strict";
 
-document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
+//document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
+
+var state = document.getElementById('s-state');
+
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
+	
+	var btnEstimate = document.getElementById('btn-estimate');
+	
+	btnEstimate.disabled = true;
+	
+	state.addEventListener('change', function() {
+	
+		if (state.value === '') {
+			btnEstimate.disabled = true;
+		} else {
+			btnEstimate.disabled = false;
+		}
+	
+	});
+});
 
 function estimateTotal(event) {
 	event.preventDefault();
