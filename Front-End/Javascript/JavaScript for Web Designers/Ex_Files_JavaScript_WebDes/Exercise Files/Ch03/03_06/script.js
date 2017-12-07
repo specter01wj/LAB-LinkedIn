87a48b06f2dc995fe2c09871cafbf1896572952a
specter01wj <specter01wj@gmail.com>
2017-12-07 01:05:38 -0500

    12/06/2017 v1

--- Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_06/script.js
+++ Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_06/script.js
@@ -6,7 +6,14 @@ document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
 function estimateTotal(event) {
 	event.preventDefault();
 	
-	console.log('You submitted the form.');
+	//console.log('You submitted the form.');
+	var state = document.getElementById('s-state');
+	
+	if (state.value === '') {
+		alert('Please choose your shipping state.');
+		
+		state.focus();
+	}
 }
 
 })();
