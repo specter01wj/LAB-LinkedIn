7771daead4bcf8d279b5c67e66ae04dcdc2ce0fe
specter01wj <specter01wj@gmail.com>
2017-12-16 19:06:55 -0500

    12/16/2017 v1

--- Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch04/04_01/script.js
+++ Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch04/04_01/script.js
@@ -1,6 +1,12 @@
 (function(){
 'use strict';
 
-
+	document.addEventListener('DOMContentLoaded', function(){
+		var c = document.getElementById('current-time');
+		
+		var d = new Date();
+		
+		c.innerHTML = d.toTimeString();
+	});
 
 })();
