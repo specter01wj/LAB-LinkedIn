5be9d5bc27544d86df9e990222fa245c7be53aac
specter01wj <specter01wj@gmail.com>
2017-12-17 22:58:43 -0500

    12/17/2017 v3

--- Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch04/04_04/script.js
+++ Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch04/04_04/script.js
@@ -9,15 +9,27 @@ document.addEventListener('DOMContentLoaded', function(){
 	function updateTime() {
 		var d = new Date();
 	
-		var hours = d.getHours();
+		var hours = d.getHours(),
+			minutes = d.getMinutes(),
+			ampm = 'AM';
+			
 		if (hours > 12) {
 			hours -= 12;
+			ampm = 'PM';
+		} else if (hours === 0) {
+			hours = 12;
 		}
 		
-		var sep = ':';
-		if (d.getSeconds() % 2 === 1) sep = ' ';
+		if (minutes < 10) {
+			minutes = '0' + minutes;
+		}
+		
+		var sepClass = '';
+		if (d.getSeconds() % 2 === 1) sepClass = 'trans';
+		
+		var sep = '<span class="' + sepClass + '">:</span>';
 	
-		c.innerHTML = hours + sep + d.getMinutes();
+		c.innerHTML = hours + sep + minutes + ' ' + ampm;
 	}
 });
 
