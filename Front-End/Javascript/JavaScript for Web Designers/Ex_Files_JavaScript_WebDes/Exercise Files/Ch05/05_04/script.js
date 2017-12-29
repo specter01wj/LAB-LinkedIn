31e1a168c88727559b452adfa97824015a95f65f
specter01wj <specter01wj@gmail.com>
2017-12-29 00:51:56 -0500

    12/28/2017 v1

--- Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch05/05_04/script.js
+++ Front-End/Javascript/JavaScript for Web Designers/Ex_Files_JavaScript_WebDes/Exercise Files/Ch05/05_04/script.js
@@ -3,7 +3,10 @@ var gMapAPIKey = 'YOUR_API_KEY_HERE';
 var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('hplus-map'), {
-		center: {lat: 34.1031131, lng: -118.326343},
-		zoom: 8
+		'center': {lat: 40.3269115, lng: -74.572301},
+		'zoom': 17,
+		'mapTypeId' : google.maps.MapTypeId.HYBRID,
+		'draggable' : true,
+		'scrollwheel' : true
 	});
 }
