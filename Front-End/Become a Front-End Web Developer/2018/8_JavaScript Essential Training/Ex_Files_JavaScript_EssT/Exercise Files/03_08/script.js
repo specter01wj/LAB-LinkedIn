8026b4a120adde984e2a7013113e3a9980400ea3
specter01wj <specter01wj@gmail.com>
2018-01-13 01:08:14 -0500

    01/12/2018 v7

--- Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/03_08/script.js
+++ Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/03_08/script.js
@@ -5,11 +5,11 @@ console.log("Before: ", pens);
 
 // PROPERTIES:
 // Get a property of an object by name:
-// console.log("Array length: ", pens.length);
+console.log("Array length: ", pens.length);
 
 // METHODS:
 // Reverse the array:
-// pens.reverse();
+pens.reverse();
 
 // Remove the first value of the array:
 // pens.shift();
@@ -24,9 +24,9 @@ console.log("Before: ", pens);
 // pens.push("pink", "prussian blue");
 
 // Find the specified position (pos) and remove n number of items from the array. Arguments: pens.splice(pos,n):
-// pens.splice(pos, n) // Starts at the seccond item and removes two items.
+pens.splice(2, 2); // Starts at the seccond item and removes two items.
 
-// console.log("After: ", pens);
+console.log("After: ", pens);
 
 // Create a copy of an array. Typically assigned to a new variable:
 // var newPens = pens.slice();
@@ -37,8 +37,8 @@ console.log("Before: ", pens);
 // console.log("The search result index is: ", result);
 
 // Return the items in an array as a comma separated string. The separator argument can be used to change the comma to something else. Arguments: pens.join(separator):
-// var arrayString = pens.join(separator);
-// console.log("String from array: ", arrayString);
+var arrayString = pens.join(' | ');
+console.log("String from array: ", arrayString);
 
 // MDN documentation for Array:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
