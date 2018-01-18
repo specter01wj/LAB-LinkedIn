f34ce4bc349554c60b8a1262f9488ae8539150af
specter01wj <specter01wj@gmail.com>
2018-01-18 01:39:32 -0500

    01/17/2018 v5

--- Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/04_05/script.js
+++ Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/04_05/script.js
@@ -1,4 +1,4 @@
-function findBiggestFraction(a,b) {
+/*function findBiggestFraction(a,b) {
     var result;
     a>b ? result = ["firstFraction", a] : result = ["secondFraction", b];
     return result;
@@ -12,3 +12,21 @@ var fractionResult = findBiggestFraction(firstFraction,secondFraction);
 console.log("First fraction result: ", firstFraction);
 console.log("Second fraction result: ", secondFraction);
 console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest!");
+*/
+var a = 3/4, b = 5/7;
+
+var theB = function() {
+
+	var res;
+
+
+	a > b ? res = ['a: ', a] : res = ['b: ', b];
+	//console.log(res);
+
+	return res;
+
+
+}
+
+var output1 = theB(a, b);
+console.log(output1);
