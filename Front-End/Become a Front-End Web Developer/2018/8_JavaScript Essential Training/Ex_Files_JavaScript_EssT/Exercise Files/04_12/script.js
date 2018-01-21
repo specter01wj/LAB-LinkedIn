12864cf0641f1b0fbb10bcdca94b758aadc88b11
specter01wj <specter01wj@gmail.com>
2018-01-21 01:29:18 -0500

    01/20/2018 v2

--- Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/04_12/script.js
+++ Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/04_12/script.js
@@ -1,11 +1,41 @@
-function doSomeMath() {
+/*function doSomeMath() {
 	var a = 5;
 	var b = 4;
-	var sum = a + b;
 
-	return sum;
+	function multiply() {
+		var result = a * b;
+		return result;
+	}
+
+	return multiply;
 }
 
 var theResult = doSomeMath();
 
-console.log("The result: ", theResult);
+console.log("The result: ", theResult());*/
+
+
+function giveMeEms(pix) {
+
+	var baseV = 16;
+
+	return function doMath() {
+		var res = pix/baseV;
+		baseV++;
+		return res;
+	}
+
+	//return doMath;
+
+}
+
+
+var smallSize = giveMeEms(12);
+
+console.log('smallSize: ' + smallSize());
+console.log('smallSize: ' + smallSize());
+console.log('smallSize: ' + smallSize());
+console.log('smallSize: ' + smallSize());
+console.log('smallSize: ' + smallSize());
+
+
