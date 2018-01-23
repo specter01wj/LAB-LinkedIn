41dae48bb9a2c4c577a49a2520a2110247ec25bc
specter01wj <specter01wj@gmail.com>
2018-01-23 02:00:23 -0500

    01/22/2018 v2

--- Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/05_06/JS/script.js
+++ Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/05_06/JS/script.js
@@ -1,1 +1,19 @@
 
+const FEATURED = document.querySelector('.story-wrapper');
+const THEIMAGE = FEATURED.querySelector('img');
+
+var altText = THEIMAGE.getAttribute('alt');
+
+var captionEle = document.createElement('figcaption');
+
+//captionEle.append(altText);
+//FEATURED.append(captionEle);
+
+var captionText = document.createTextNode(altText);
+
+captionEle.appendChild(captionText);
+
+FEATURED.appendChild(captionEle);
+
+THEIMAGE.setAttribute('alt','');
+
