ac3b5ff5c527384c9b2a0bf160fe6fe6eb26269f
specter01wj <specter01wj@gmail.com>
2018-01-24 01:28:58 -0500

    01/23/2018 v2

--- Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/06_03/script.js
+++ Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/06_03/script.js
@@ -1,3 +1,9 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let hrPos = 20, minPos = 130, secPos = 267;
+
+HOURHAND.style.transform = 'rotate(' + hrPos + 'deg)';
+MINUTEHAND.style.transform = 'rotate(' + minPos + 'deg)';
+SECONDHAND.style.transform = 'rotate(' + secPos + 'deg)';
