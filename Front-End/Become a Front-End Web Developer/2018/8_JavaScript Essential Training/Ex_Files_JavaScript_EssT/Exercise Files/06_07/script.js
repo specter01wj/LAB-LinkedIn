5056dc167506d93fadfe84c294139b7ce412e452
specter01wj <specter01wj@gmail.com>
2018-01-26 01:21:34 -0500

    01/25/2018 v2

--- Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/06_07/script.js
+++ Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/06_07/script.js
@@ -2,17 +2,24 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-function runTheClock() {
     var date = new Date();
-    console.log(date);
+    //console.log(date);
     let hr = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
-    console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+    //console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
     let hrPosition = (hr*360/12)+(min*(360/60)/12);
     let minPosition = (min*360/60)+(sec*(360/60)/60);
     let secPosition = sec*360/60;
+
+
+
+function runTheClock() {
+
+    hrPosition = hrPosition + (3/360);
+    minPosition = minPosition + (6/60);
+    secPosition = secPosition + 6;
 
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
