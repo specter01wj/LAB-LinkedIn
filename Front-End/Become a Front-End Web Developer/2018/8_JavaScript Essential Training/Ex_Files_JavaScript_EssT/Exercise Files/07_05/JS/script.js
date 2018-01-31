be9d9c49baacfd4505145c404c0948b895ba383d
specter01wj <specter01wj@gmail.com>
2018-01-31 01:35:58 -0500

    01/30/2018 v3

--- Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/07_05/JS/script.js
+++ Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/07_05/JS/script.js
@@ -4,11 +4,12 @@ const ALERT = document.querySelector("#booking-alert");
 CTA.classList.remove("hide");
 ALERT.classList.add("hide");
 
-function reveal(e) {
+function reveal(e, curr) {
     e.preventDefault();
-    CTA.classList.toggle("hide");
+    curr.innerHTML == 'Book Now!' ? CTA.innerHTML = 'Oooops!' : CTA.innerHTML = 'Book Now!';
+    //CTA.classList.toggle("hide");
     ALERT.classList.toggle("hide");
 }
 
-CTA.addEventListener('click', reveal, false);
+CTA.addEventListener('click', function(e){reveal(e, this)}, false);
 CTA.addEventListener('click', function(){console.log("The button was clicked!")}, false);
