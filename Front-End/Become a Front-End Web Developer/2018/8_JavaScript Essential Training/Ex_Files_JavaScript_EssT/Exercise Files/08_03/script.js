31a653299a9f0aa53f2f0d0bbe7f0fb3e9498dfa
specter01wj <specter01wj@gmail.com>
2018-02-04 14:22:28 -0500

    02/04/2018 v2

--- Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/08_03/script.js
+++ Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/08_03/script.js
@@ -12,12 +12,30 @@ const theTimer = document.querySelector(".timer");
 
 
 // Match the text entered with the provided text on the page:
+function spellCheck() {
+	let textEnter = testArea.value;
+	console.log(textEnter);
+}
 
 
 // Start the timer:
+function start() {
+	let textEnterLen = testArea.value.length;
+	console.log(textEnterLen);
+}
 
 
 // Reset everything:
+function reset() {
+	console.log('reset button.');
+}
 
 
 // Event listeners for keyboard input and the reset button:
+
+testArea.addEventListener('keypress', start, false);
+
+testArea.addEventListener('keyup', spellCheck, false);
+
+resetButton.addEventListener('click', reset, false);
+
