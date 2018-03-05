02c65869ca81c11f60381c91dd9c208161154c12
specter01wj <specter01wj@gmail.com>
2018-03-05 01:52:46 -0500

    03/04/2018 v1

--- Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/09_03/JS/primeScript.js
+++ Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/09_03/JS/primeScript.js
@@ -8,9 +8,24 @@ function primeTest(testValue) {
         }
     }
     return isPrime;
+    /*var divisor = 2;
+
+	  while (testValue > divisor){
+	    if(testValue % divisor === 0){
+	     return false; 
+	    }
+	    else
+	      divisor++;
+	  }
+	  return true;*/
 }
 
 for (let i = 2; i<=CEILING; i++) {
     let result = primeTest(i);
+    /*if(result===true){
+    	console.log(i + " is a prime number.");
+    }*/
+    if(result===false) continue;
     console.log(i + " is a prime number.");
+    //result === true ? console.log(i + " is a prime number.") : console.log(i + " is NOT a prime number.");
 }
