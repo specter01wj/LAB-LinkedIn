0827540bccf9ca2b9444feb4227ceab7afa18332
specter01wj <specter01wj@gmail.com>
2018-03-04 21:49:08 -0500

    03/04/2018 v2

--- Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/09_02/JS/script.js
+++ Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/09_02/JS/script.js
@@ -1,1 +1,10 @@
 
+var extLinks = document.querySelectorAll('a[href^="http"]');
+//console.log(extLinks);
+
+for( var i = 0; i < extLinks.length; i++) {
+	if( !extLinks[i].hasAttribute('target') ) {
+		extLinks[i].setAttribute('target', '_blank');
+	}
+}
+
