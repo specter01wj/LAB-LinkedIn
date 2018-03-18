7c664640950018a07db224e31e28ad71190c4235
specter01wj <specter01wj@gmail.com>
2018-03-18 18:35:03 -0400

    03/18/2018 v2

--- Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/10_05/JS/script.js
+++ Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/10_05/JS/script.js
@@ -1,5 +1,12 @@
 const IMAGES = document.querySelectorAll("img");
 
+const SIZES = {
+    showcase: '100vw',
+    reason: '(max-width: 799px) 100vw, 372px',
+    feature: '(max-width: 799px) 100vw, 558px',
+    story: '(max-width: 799px) 100vw, 670px'
+}
+
 function makeSrcset(imgSrc) {
     let markup = [];
     let width = 400;
@@ -19,5 +26,6 @@ for (let i = 0; i<IMAGES.length; i++) {
     console.log(srcset);
 
     let type = IMAGES[i].getAttribute("data-type");
-    console.log(type);
+    let sizes = SIZES[type];
+    console.log(sizes);
 }
