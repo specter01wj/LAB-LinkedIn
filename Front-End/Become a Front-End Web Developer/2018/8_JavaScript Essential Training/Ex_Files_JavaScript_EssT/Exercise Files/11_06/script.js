1e4a3d9dd98526e85ac3d5f5071ad8a1233cbe18
specter01wj <specter01wj@gmail.com>
2018-03-25 22:25:26 -0400

    03/25/2018 ve

--- Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/11_06/script.js
+++ Front-End/Become a Front-End Web Developer/2018/8_JavaScript Essential Training/Ex_Files_JavaScript_EssT/Exercise Files/11_06/script.js
@@ -4,7 +4,7 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-timer = [0,0,0,0]
+var timer = [0,0,0,0];
 var interval;
 var timerRunning = false;
 
