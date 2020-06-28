2dbe7e7b09e463158882260e9998684aa172a950
specter01wj <specter01wj@gmail.com>
2020-06-28 17:53:02 -0600

    06/04/2020 v1

--- Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_02/mysol_1/02_debug.js
+++ Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_02/mysol_1/02_debug.js
@@ -1,13 +1,13 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 02, Exercise 02
 
-function debug(message) {
+/*function debug(message) {
   console.log("DEBUG: " + message)
 }
 
 // ...
 
-debug("Some debug message")
+debug("Some debug message")*/
 
 // ...
 
@@ -16,3 +16,23 @@ debug("Some debug message")
 // }
 //
 // function doNothing() { }
+
+const DEBUG_MODE_ENABLED = true;
+
+let debug;
+
+if (DEBUG_MODE_ENABLED) {
+  debug = printDebugMessage
+} else {
+  debug = doNothing
+}
+
+debug("Some debug message");
+
+function printDebugMessage(message) {
+  console.log("DEBUG: " + message)
+}
+
+function doNothing() { 
+	console.log("Did nothing!")
+}
