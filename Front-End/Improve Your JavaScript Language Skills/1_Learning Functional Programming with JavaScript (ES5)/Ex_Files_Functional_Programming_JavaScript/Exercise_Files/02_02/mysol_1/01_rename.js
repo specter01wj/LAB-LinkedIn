e93d4c70737a2131398914cfedb2ad720eb52bb6
specter01wj <specter01wj@gmail.com>
2020-06-28 13:20:51 -0600

    06/04/2020 v1

---
+++ Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_02/mysol_1/01_rename.js
@@ -0,0 +1,15 @@
+// Learning Functional Programming with Javascript
+// Chapter 02, Video 02, Exercise 01
+
+console.log()
+console.log("Hello and welcome to the course!")
+console.log("We seem to be using")
+console.log("a lot of console.logs here.")
+console.log("Why don't we rename it")
+console.log("to make it look better?")
+console.log("To do that, simply assign the function")
+console.log("'console.log' to another variable.")
+console.log("You can call it whatever you want.")
+console.log("I called mine 'line' to reflect the")
+console.log("way that I'm using it.")
+console.log()
