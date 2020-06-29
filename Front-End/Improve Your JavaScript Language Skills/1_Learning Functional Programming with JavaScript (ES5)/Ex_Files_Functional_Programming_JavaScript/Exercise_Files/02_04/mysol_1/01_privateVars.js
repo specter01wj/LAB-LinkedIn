e036e60853a9db43a14f6be7d109621c2aa084f8
specter01wj <specter01wj@gmail.com>
2020-06-28 22:15:35 -0600

    06/04/2020 v1

--- Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_04/mysol_1/01_privateVars.js
+++ Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_04/mysol_1/01_privateVars.js
@@ -1,7 +1,7 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 04, Exercise 01
 
-const myCounter = {
+/*const myCounter = {
   count: 0,
   increment: function() {
     this.count += 1
@@ -20,4 +20,28 @@ console.log(myCounter.currentValue())
 
 myCounter.count = 999 // uh oh!
 
-console.log(myCounter.currentValue())
+console.log(myCounter.currentValue())*/
+
+
+function createCounter() {
+  var count = 0;
+  return {
+  	increment: function() {
+	    count += 1;
+	  },
+	  currentValue: function() {
+	    return count;
+	  }
+  }
+}
+
+var myCounter = createCounter();
+
+console.log(myCounter.currentValue());
+
+myCounter.increment();
+myCounter.increment();
+
+console.log(myCounter.currentValue());
+
+console.log(myCounter.currentValue());
