09f24b1a5a13decf47ca8c50e7b91259c19e5527
specter01wj <specter01wj@gmail.com>
2020-06-28 21:54:21 -0600

    06/04/2020 v1

--- Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_03/mysol_1/03_ifElse.js
+++ Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_03/mysol_1/03_ifElse.js
@@ -1,8 +1,38 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 03, Exercise 03
 
-function doIf(condition, func) {
+/*function doIf(condition, func) {
   if (condition) {
     func()
   }
+}*/
+
+function ifElse(condition, func1, func2) {
+	if(condition) {
+		func1();
+	} else {
+		func2();
+	}
 }
+
+
+let x = 1;
+
+
+ifElse(x === 1, function() {
+	console.log('x is 1');
+}, function() {
+	console.log('x is not 1');
+});
+
+
+
+
+
+
+
+
+
+
+
+
