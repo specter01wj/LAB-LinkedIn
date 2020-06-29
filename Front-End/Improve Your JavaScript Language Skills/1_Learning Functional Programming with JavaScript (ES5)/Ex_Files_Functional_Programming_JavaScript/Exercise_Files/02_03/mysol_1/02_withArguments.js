e9caf602994fde675bff4d644495f9497ecec241
specter01wj <specter01wj@gmail.com>
2020-06-28 21:51:13 -0600

    06/04/2020 v1

--- Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_03/mysol_1/02_withArguments.js
+++ Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_03/mysol_1/02_withArguments.js
@@ -1,6 +1,29 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 03, Exercise 02
 
+/*function add(x, y) {
+  return x + y
+}
+
+function subtract(x, y) {
+  return x - y
+}*/
+
+function callWith2Arguments(arg1, arg2, func) {
+	return func(arg1, arg2);
+}
+
+
+callWith2Arguments(1, 2, add);
+callWith2Arguments(9, 4, subtract);
+
+
+callWith2Arguments(2, 4, function(x, y) {
+	return x * x + y * y;
+});
+
+
+
 function add(x, y) {
   return x + y
 }
@@ -8,3 +31,8 @@ function add(x, y) {
 function subtract(x, y) {
   return x - y
 }
+
+
+
+
+
