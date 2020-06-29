7187838eb730dbeb0de36984c5515a6539ca2bf6
specter01wj <specter01wj@gmail.com>
2020-06-28 22:43:03 -0600

    06/04/2020 v1

--- Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_04/mysol_1/02_capturingArguments.js
+++ Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_04/mysol_1/02_capturingArguments.js
@@ -1,7 +1,7 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 04, Exercise 02
 
-function createCounter() {
+/*function createCounter() {
   var count = 0
 
   return {
@@ -13,4 +13,25 @@ function createCounter() {
       return count
     }
   }
+}*/
+
+
+function createCounter(count) {
+
+  return {
+    increment: function() {
+      count = count + 1
+    },
+
+    currentValue: function() {
+      return count
+    }
+  }
 }
+
+let counterStartingAt5 = createCounter(5);
+
+let counterStartingAtMinus2 = createCounter(-2);
+
+
+
