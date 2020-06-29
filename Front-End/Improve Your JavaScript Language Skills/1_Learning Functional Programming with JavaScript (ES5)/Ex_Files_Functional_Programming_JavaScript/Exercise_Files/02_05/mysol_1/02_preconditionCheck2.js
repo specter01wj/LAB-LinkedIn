19256b15fd02941b7b9806d1f2639c8e3d4ce0fd
specter01wj <specter01wj@gmail.com>
2020-06-28 22:59:59 -0600

    06/04/2020 v1

--- Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_05/mysol_1/02_preconditionCheck2.js
+++ Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_05/mysol_1/02_preconditionCheck2.js
@@ -1,7 +1,7 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 05, Exercise 02
 
-function doIfSafe(n, message, func) {
+/*function doIfSafe(n, message, func) {
   if (n != null && typeof n === 'number') {
     if (message != null && typeof message === 'string') {
       return func(n, message)
@@ -23,4 +23,43 @@ function getSubstringOfLength(n, string) {
 
 doIfSafe(4, "Banana", printMessageNTimes) // prints "Banana Banana Banana Banana"
 doIfSafe(2, "Javascript", getNthLetter) // 'v'
-doIfSafe(5, "Hello and welcome", getSubstringOfLength) // "Hello"
+doIfSafe(5, "Hello and welcome", getSubstringOfLength) // "Hello"*/
+
+
+function createSafeVersion(func) {
+  return function(n, message) {
+    if (n != null && typeof n === 'number') {
+      if (message != null && typeof message === 'string') {
+        return func(n, message);
+      }
+    }
+  }
+}
+
+function printMessageNTimes(n, message) {
+  for (var i = 0; i < n; i++) { console.log(message) }
+}
+
+function getNthLetter(n, string) {
+  return string.charAt(n);
+}
+
+function getSubstringOfLength(n, string) {
+  return string.substring(0, n);
+}
+
+
+var printMessageNTimesSafe = createSafeVersion(printMessageNTimes);
+var getNthLetterSafe = createSafeVersion(getNthLetter);
+var getSubstringOfLengthSafe = createSafeVersion(getSubstringOfLength);
+
+
+printMessageNTimesSafe(4, "Banana"); // prints "Banana Banana Banana Banana"
+getNthLetterSafe(2, "Javascript"); // 'v'
+getSubstringOfLengthSafe(5, "Hello and welcome"); // "Hello"
+
+getNthLetterSafe("two", "Javascript");
+
+
+
+
