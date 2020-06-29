301b0ba43d7e2aea34191674eb9794fb93ee5d51
specter01wj <specter01wj@gmail.com>
2020-06-28 21:46:22 -0600

    06/04/2020 v1

--- Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_03/mysol_1/01_doif.js
+++ Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_03/mysol_1/01_doif.js
@@ -1,7 +1,7 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 03, Exercise 01
 
-const x = 1
+/*const x = 1
 
 if (x === 1) {
   sayXis1()
@@ -20,3 +20,36 @@ function sayXis1() { console.log("x is equal to 1") }
 function sayXisBananas() { console.log("x is equal to 'Bananas'") }
 
 function sayXisBetween0And10() { console.log("x is between 0 and 10") }
+*/
+
+function doIf(condition, func) {
+	if(condition) {
+		func();
+	}
+}
+
+const x = 1;
+
+doIf(x === 1, function() {
+	console.log('x is equal to 1');
+});
+
+doIf(x === 'Bananas', function() {
+	console.log('x is equal to Bananas');
+});
+
+doIf(x < 10 && x > 0, function() {
+	console.log('x is between 0 and 10');
+});
+
+
+
+
+
+
+
+
+
+
+
+
