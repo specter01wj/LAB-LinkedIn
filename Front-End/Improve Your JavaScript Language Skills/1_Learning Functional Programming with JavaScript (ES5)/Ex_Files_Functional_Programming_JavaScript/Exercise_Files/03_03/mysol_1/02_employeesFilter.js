e6de27a176c9486e5f2fb9af0cdc20b293d03719
specter01wj <specter01wj@gmail.com>
2020-06-30 01:18:09 -0600

    06/04/2020 v1

--- Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/03_03/mysol_1/02_employeesFilter.js
+++ Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/03_03/mysol_1/02_employeesFilter.js
@@ -1,7 +1,7 @@
 // Learning Functional Programming with Javascript
 // Chapter 03, Video 03, Exercise 02
 
-var employees = [
+/*var employees = [
   { name: "John",  salary: 50000  },
   { name: "Susan", salary: 60000  },
   { name: "Greg",  salary: 100000 },
@@ -15,4 +15,30 @@ for (var i = 0; i < employees.length; i++) {
   if (employee.salary < 70000) {
     dueForARaise.push(employee)
   }
-}
+}*/
+
+let _ = require("lodash");
+
+let employees = [
+  { name: "John",  salary: 50000  },
+  { name: "Susan", salary: 60000  },
+  { name: "Greg",  salary: 100000 },
+  { name: "Mary",  salary: 120000 }
+];
+
+var dueForARaise = _.filter(employees, function(emp) {
+	return emp.salary < 70000;
+});
+
+console.log(dueForARaise);
+
+
+let dueForARaise2 = employees.filter( x => x.salary < 70000);
+
+console.log(dueForARaise2);
+
+
+
+
+
+
