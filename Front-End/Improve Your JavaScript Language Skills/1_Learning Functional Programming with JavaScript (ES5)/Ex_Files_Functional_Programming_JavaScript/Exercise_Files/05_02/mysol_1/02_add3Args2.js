3cfc3595fcb2bb4cca88e06f564921df03469b71
specter01wj <specter01wj@gmail.com>
2020-06-30 12:40:53 -0600

    06/04/2020 v1

--- Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/05_02/mysol_1/02_add3Args2.js
+++ Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/05_02/mysol_1/02_add3Args2.js
@@ -1,7 +1,7 @@
 // Learning Functional Programming with Javascript
 // Chapter 05, Video 02, Exercise 02
 
-function add(x, y, z) {
+/*function add(x, y, z) {
   return x + y + z
 }
 
@@ -14,4 +14,25 @@ function partiallyApply(func, x) {
 var add5 = partiallyApply(add, 5)
 
 console.log(add5(2, 3))
+console.log(add(5, 2, 3))*/
+
+
+function add(x, y, z) {
+  return x + y + z
+}
+
+function partiallyApply(func, x, y) {
+  return function(z) {
+    return func(x, y, z)
+  }
+}
+
+var add5and2 = partiallyApply(add, 5, 2)
+
+console.log(add5and2(3))
 console.log(add(5, 2, 3))
+
+
+
+
+
