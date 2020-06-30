852b8a6c6f09076e1562382c23142248adf4b783
specter01wj <specter01wj@gmail.com>
2020-06-30 01:01:31 -0600

    06/04/2020 v1

--- Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/03_02/mysol_1/01_simpleMap.js
+++ Front-End/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript (ES5)/Ex_Files_Functional_Programming_JavaScript/Exercise_Files/03_02/mysol_1/01_simpleMap.js
@@ -1,10 +1,30 @@
 // Learning Functional Programming with Javascript
 // Chapter 03, Video 02, Exercise 01
 
-var numbers = [ 1, 2, 3, 4, 5 ]
+/*var numbers = [ 1, 2, 3, 4, 5 ]
 var numbersCubed = [ ]
 
 for(var i = 0; i < numbers.length; i++) {
   var element = numbers[i]
   numbersCubed.push(element * element * element)
-}
+}*/
+
+let _ = require('lodash');
+
+let numbers = [ 1, 2, 3, 4, 5 ];
+
+let numbersCubed = _.map(numbers, function(elem) {
+	return elem * elem * elem;
+});
+
+console.log(numbersCubed);
+
+
+let numbersCubed2 = numbers.map( (x) => {
+	return	x * x * x;
+});
+
+console.log(numbersCubed2);
+
+
+
