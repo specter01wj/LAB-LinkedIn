f467824dfede9ba19ca10d7ef46984508ee3a5ab
specter01wj <specter01wj@gmail.com>
2020-07-01 23:57:20 -0600

    06/04/2020 v1

--- Front-End/Improve Your JavaScript Language Skills/4_JavaScript - Classes/Ex_Files_JavaScript_Classes/Exercise Files/CH1/01_02/mysol_1/app.js
+++ Front-End/Improve Your JavaScript Language Skills/4_JavaScript - Classes/Ex_Files_JavaScript_Classes/Exercise Files/CH1/01_02/mysol_1/app.js
@@ -2,19 +2,6 @@ class Car {
     constructor(doors, engine, color) {
         this.doors = doors;
         this.engine = engine;
-
-
-
-
-
-
-
-
-
-
-
-
-        
         this.color = color;
     }
 
