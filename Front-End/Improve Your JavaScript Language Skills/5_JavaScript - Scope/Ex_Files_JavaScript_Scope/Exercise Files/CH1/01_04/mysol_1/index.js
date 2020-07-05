39fa3e73f7be5f2f21b9f268ea1079364ccaadcd
specter01wj <specter01wj@gmail.com>
2020-07-04 19:48:12 -0600

    06/04/2020 v1

---
+++ Front-End/Improve Your JavaScript Language Skills/5_JavaScript - Scope/Ex_Files_JavaScript_Scope/Exercise Files/CH1/01_04/mysol_1/index.js
@@ -0,0 +1,18 @@
+var warrior = 'Ninja';
+
+const screamWarrior = () => {
+    let warrior2 = 'Samurai';
+    return {
+        shootWarrior: () => {
+            return console.log(warrior, warrior2);
+            
+        }
+    }
+}
+
+const newWarrior = screamWarrior();
+
+newWarrior.shootWarrior();
+
+console.log(warrior, warrior2);
+
