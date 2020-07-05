d5f0ee6c7d7ee8394b9d881979b0b58ba0dcd0eb
specter01wj <specter01wj@gmail.com>
2020-07-05 13:35:25 -0600

    06/04/2020 v1

---
+++ Front-End/Improve Your JavaScript Language Skills/5_JavaScript - Scope/Ex_Files_JavaScript_Scope/Exercise Files/CH3/03_03/mysol_1/index.js
@@ -0,0 +1,19 @@
+
+let numberOfPlatoon = 55;
+
+
+const platoon = () => {
+	let warriorsPerPlatoon = 66;
+	let totalWarriors = numberOfPlatoon * warriorsPerPlatoon;
+
+	return `Our army is ${totalWarriors} strong!`;
+}
+
+
+console.log(platoon());
+
+
+console.log(numberOfPlatoon);
+
+
+
