ba1e2423c822a91f9685140d4b0b53731ca92c32
specter01wj <specter01wj@gmail.com>
2020-07-05 17:38:37 -0600

    06/04/2020 v1

---
+++ Front-End/Improve Your JavaScript Language Skills/5_JavaScript - Scope/Ex_Files_JavaScript_Scope/Exercise Files/CH4/04_04/mysol_1/index.js
@@ -0,0 +1,48 @@
+
+const warriors = [
+    {
+        name: 'Jujin Take',
+        type: 'Ninja',
+        weapon: 'Shuriken',
+        agility: 79
+    },
+    {
+        name: 'Viggo Rusen',
+        type: 'Viking',
+        weapon: 'Swing blade',
+        agility: 60
+    },
+    {
+        name: 'Ro Rake',
+        type: 'Samurai',
+        weapon: 'Boken',
+        agility: 82
+    }
+];
+
+
+const screamWarriors = () => {
+    warriors.map( (warrior) => {
+        if(warrior.agility === 60) {
+            let agility = warrior.agility;
+            console.log(`${warrior.name}, who is a ${warrior.type} has an agility of ${agility}`);
+        }
+    });
+}
+
+
+screamWarriors();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
