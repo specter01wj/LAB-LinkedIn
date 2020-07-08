c51f1b4b32a85b7e7824e1eb4a151fc5fc9ea5d7
specter01wj <specter01wj@gmail.com>
2020-07-07 18:47:00 -0600

    06/04/2020 v1

---
+++ Front-End/Improve Your JavaScript Language Skills/7_JavaScript - Patterns/Ex_Files_JavaScript_Patterns/Exercise Files/CH1/01_04/mysol_1/index.js
@@ -0,0 +1,19 @@
+/*const calc = () => {
+    return 4 * 3;
+}
+
+let aNumber = calc();
+
+console.log(aNumber);*/
+
+
+const calc = () => {
+    return 4 * 15;
+}
+
+
+const printCalc = (callback) => {
+	console.log(callback);
+}
+
+printCalc(calc());
