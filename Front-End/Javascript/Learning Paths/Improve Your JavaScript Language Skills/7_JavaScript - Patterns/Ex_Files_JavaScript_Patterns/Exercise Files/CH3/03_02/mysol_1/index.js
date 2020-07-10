29f08ab6a5e1267cab817922644f9d7d03267b75
specter01wj <specter01wj@gmail.com>
2020-07-09 18:08:28 -0600

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/7_JavaScript - Patterns/Ex_Files_JavaScript_Patterns/Exercise Files/CH3/03_02/mysol_1/index.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/7_JavaScript - Patterns/Ex_Files_JavaScript_Patterns/Exercise Files/CH3/03_02/mysol_1/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import calc from './calc';
 
+const aNum = calc();
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
+    res.send(`Showing number ${aNum} on port ${PORT}`)
 );
 
 app.listen(PORT, () =>
