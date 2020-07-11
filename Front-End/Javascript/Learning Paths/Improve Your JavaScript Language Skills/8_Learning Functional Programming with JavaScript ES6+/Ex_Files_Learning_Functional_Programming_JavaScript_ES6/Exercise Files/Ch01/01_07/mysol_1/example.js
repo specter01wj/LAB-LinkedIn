a62d035229aa27a69c89c6cf73b38a241d64ad08
specter01wj <specter01wj@gmail.com>
2020-07-11 15:35:07 -0600

    06/04/2020 v1

---
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch01/01_07/mysol_1/example.js
@@ -0,0 +1,19 @@
+
+const x = 5;
+// x = 6;
+
+const num1 = [1,2,3,4,5];
+num1[0] = 100;
+num1.reverse();
+
+console.log(num1);
+
+const person = {
+    name: 'James',
+    age: 34
+};
+
+// person = { name: 'Bob'};
+person.name = 'Bob';
+
+console.log(person);
