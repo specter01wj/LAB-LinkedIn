74f6d79e5f462c7fad9cd4d6b1538f8908b84c7a
specter01wj <specter01wj@gmail.com>
2020-07-12 22:05:57 -0600

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_01/mysol_1/examples.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_01/mysol_1/examples.js
@@ -1,1 +1,37 @@
 
+function myFun(arg1, arg2) {
+
+}
+
+const myOtherFun = function(arg1, arg2) {
+
+}
+
+const add = (x, y) => x + y;
+
+const double = number => number * 2;
+
+const double2 = function(number) {
+    return number * 2;
+}
+
+const sayHello = () => console.log('Hello');
+
+const getPersonData = () => (
+    {
+        name: 'John Doe',
+        age: 34,
+        job: 'programmer'
+    }
+);
+
+const myArrowFunction = (arg1, arg2) => ({
+    name: 'John Doe',
+});
+
+const myArrowFun2 = (arg1, arg2) => {
+    return (
+        { name: 'John Doe' }
+    )
+};
+
