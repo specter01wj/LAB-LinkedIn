396915462460de675351099264571e0dc3351111
specter01wj <specter01wj@gmail.com>
2020-07-12 22:38:55 -0600

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_04/mysol_1/examples.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_04/mysol_1/examples.js
@@ -1,2 +1,17 @@
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
+
+// const combine2and3 = func => func(2, 3);
+
+const combine2and3 = (func) => {
+    return func(2, 3);
+}
+
+console.log(combine2and3(add));
+
+console.log(combine2and3((x, y) => x + y));
+
+console.log(combine2and3(subtract));
+
+console.log(combine2and3(Math.max));
+
