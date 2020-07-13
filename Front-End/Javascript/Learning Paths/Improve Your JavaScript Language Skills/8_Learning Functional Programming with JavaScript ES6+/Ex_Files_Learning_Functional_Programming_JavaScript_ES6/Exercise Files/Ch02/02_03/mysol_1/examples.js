29e349e10d54b16906992049180b20e0d635ee94
specter01wj <specter01wj@gmail.com>
2020-07-12 22:30:23 -0600

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_03/mysol_1/examples.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_03/mysol_1/examples.js
@@ -2,3 +2,27 @@ const double = x => x * 2;
 const subtractOne = x => x - 1;
 const triple = x => x * 3;
 const add5 = x => x + 5;
+
+
+const myNumber = 42;
+
+const doubled = double(myNumber);
+const minusOne = subtractOne(doubled);
+
+const functionsArray = [
+    double,
+    subtractOne,
+    triple,
+    add5,
+    Math.sqrt,
+];
+
+var number = 42;
+
+// functionsArray.forEach(func => number = func(number));
+
+functionsArray.forEach( (func) => {
+    return number = func(number);
+});
+
+console.log(number);
