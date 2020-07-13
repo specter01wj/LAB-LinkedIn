bac19285d3d63f5db030b35b0b72e13e4035553e
specter01wj <specter01wj@gmail.com>
2020-07-13 13:48:56 -0600

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_05/mysol_1/examples.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_05/mysol_1/examples.js
@@ -10,5 +10,12 @@ const createPrinter2 = function() {
 createPrinter()();
 createPrinter2()();
 
+const createMultiplier = y => x => x * y;
 
+const double = createMultiplier(2);
+const triple = createMultiplier(3);
+const quadruple = createMultiplier(4);
 
+console.log(double(3));
+console.log(triple(3));
+console.log(quadruple(3));
