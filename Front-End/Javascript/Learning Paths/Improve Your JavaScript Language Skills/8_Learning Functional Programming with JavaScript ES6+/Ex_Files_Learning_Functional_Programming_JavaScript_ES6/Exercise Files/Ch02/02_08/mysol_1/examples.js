56fbee46596e6252c079bb0741deea642f6e5128
specter01wj <specter01wj@gmail.com>
2020-07-13 14:29:37 -0600

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_08/mysol_1/examples.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_08/mysol_1/examples.js
@@ -9,18 +9,26 @@ const divide = (x, y) => {
 }
 
 console.log(divide(7, 0));
+console.log(divide(5, 3));
+
 
 const divide2 = (x, y) => x / y;
 
+const secondArgumentIsntZero = (func) => {
+    return (...args) => {
+        if (args[1] === 0) {
+            console.log('Error: dividing by zero');
+            return null;
+        }
 
+        return func(...args);
+    }
+}
 
+const divideSafe = secondArgumentIsntZero(divide2);
 
-
-
-
-
-
-
+console.log(divideSafe(7, 0));
+console.log(divideSafe(5, 3));
 
 
 
