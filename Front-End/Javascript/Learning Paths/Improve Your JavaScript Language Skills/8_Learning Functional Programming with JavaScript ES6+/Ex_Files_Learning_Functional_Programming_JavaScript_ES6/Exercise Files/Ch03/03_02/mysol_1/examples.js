8cb8dd9dbd77363d323cf19bf778e54e72deaf14
specter01wj <specter01wj@gmail.com>
2020-07-15 00:58:53 -0600

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch03/03_02/mysol_1/examples.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch03/03_02/mysol_1/examples.js
@@ -1,14 +1,24 @@
-// const person = {
-//     name: 'Jimmy Smith',
-//     age: 40,
-//     hairColor: 'brown',
-//     eyeColor: 'blue',
-// };
+const person = {
+    name: 'Jimmy Smith',
+    age: 40,
+    hairColor: 'brown',
+    eyeColor: 'blue',
+    company: 'IBM',
+};
 
-// const careerData = {
-//     title: 'developer',
-//     company: 'ABC Software',
-// };
+const careerData = {
+    title: 'developer',
+    company: 'ABC Software',
+};
+
+
+const personWithCareerData = {
+    ...person,
+    ...careerData
+};
+
+console.log(personWithCareerData);
+
 
 
 
