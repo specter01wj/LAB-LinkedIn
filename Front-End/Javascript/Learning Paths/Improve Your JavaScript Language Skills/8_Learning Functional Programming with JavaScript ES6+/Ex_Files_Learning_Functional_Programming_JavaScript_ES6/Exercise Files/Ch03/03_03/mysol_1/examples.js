4ab7167a70579221bd24792cf182d9dfc1cb915d
specter01wj <specter01wj@gmail.com>
2020-07-15 01:10:57 -0600

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch03/03_03/mysol_1/examples.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch03/03_03/mysol_1/examples.js
@@ -1,1 +1,22 @@
 const numbers = [1, 2, 3, 4, 5];
+
+numbers.map(x => x * x);
+
+console.log(numbers);
+
+numbers.reverse();
+
+console.log(numbers);
+
+const numbers2 = [1, 2, 3, 4, 5];
+
+const doubledNumbers = numbers2.map( (x) => {
+    return x * 2;
+});
+
+console.log(doubledNumbers);
+
+
+
+
+
