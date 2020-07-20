d83b040dc3cb717c4b5cbce6bef631651712e31d
specter01wj <specter01wj@gmail.com>
2020-07-19 22:46:03 -0400

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch03/03_07/mysol_1/examples.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch03/03_07/mysol_1/examples.js
@@ -1,1 +1,14 @@
 const mixedUpNumbers = [10, 2, 4, 3, 7, 5, 8, 9, 1, 6];
+
+const ascending = (a, b) => {
+    if(a < b) return -1;
+    if(a > b) return 1;
+    return 0;
+}
+
+let sortedNum = mixedUpNumbers.slice().sort(ascending);
+
+
+console.log(sortedNumbers);
+console.log(mixedUpNumbers);
+
