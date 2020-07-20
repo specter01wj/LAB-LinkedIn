48800f47b7519c71dcea9b52444e6735f8437397
specter01wj <specter01wj@gmail.com>
2020-07-19 23:07:22 -0400

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch03/03_08/mysol_1/examples.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch03/03_08/mysol_1/examples.js
@@ -1,1 +1,10 @@
 const numbers = [5, 7, 2, 40, 23, 14, 8, 4, 11];
+
+const product = numbers.reduce( (acc, elem) => {
+    console.log(`acc is ${acc}`);
+    console.log(`x is ${elem}`);
+    return acc + elem;
+}, 0);
+
+console.log(product);
+
