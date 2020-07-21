3652ef0b77fd9b4d115058eb887d4c79f47c0807
specter01wj <specter01wj@gmail.com>
2020-07-21 02:06:12 -0400

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch05/05_03/mysol_1/challenge.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch05/05_03/mysol_1/challenge.js
@@ -1,5 +1,19 @@
 import words from 'an-array-of-english-words';
 
+const countOccurrences = arr =>
+    arr.reduce((acc, str) => ({
+        ...acc,
+        [str]: acc[str] ? acc[str] + 1 : 1,
+    }), {});
+
+const hasSameLetterCount = (word1, word2) => {
+    const word1Count = countOccurrences(word1.split(''));
+    const word2Count = countOccurrences(word2.split(''));
+
+    return Object.keys(word1Count).length === Object.keys(word2Count).length
+        && Object.keys(word1Count).every(letter => word1Count[letter] === word2Count[letter]);
+}
+
 const findAnagrams = (word, allWords) => {
     const letters = word.split('');
 
