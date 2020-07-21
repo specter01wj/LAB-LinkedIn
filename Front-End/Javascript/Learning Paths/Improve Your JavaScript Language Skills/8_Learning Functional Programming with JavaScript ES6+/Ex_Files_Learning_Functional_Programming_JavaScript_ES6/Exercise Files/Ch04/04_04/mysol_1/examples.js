709d8e7d36ccc6c58686d2cb34b5142411fbfce0
specter01wj <specter01wj@gmail.com>
2020-07-21 00:59:22 -0400

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch04/04_04/mysol_1/examples.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/8_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch04/04_04/mysol_1/examples.js
@@ -3,12 +3,15 @@ const sayHello = () => console.log('Hello');
 console.log(sayHello.name);
 
 
+const add = (x, y, z) => x + y + z;
+console.log(add.length);
+console.log(add.toString());
+console.log(add.call(this, 1, 2, 3));
+console.log(add.apply(this, [1, 2, 3]));
 
 
-
-
-
-
+const args = [1, 2, 3];
+console.log(add(...args));
 
 
 
