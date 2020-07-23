c4ae038fd50807a0edd315a0212d32ca2f276d47
specter01wj <specter01wj@gmail.com>
2020-07-23 02:09:57 -0400

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/5_JavaScript - Best Practices for Data/Ex_Files_JavaScript_BP_Data/Exercise Files/Ch02/02_02/mysol_1/app.js
+++ Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/5_JavaScript - Best Practices for Data/Ex_Files_JavaScript_BP_Data/Exercise Files/Ch02/02_02/mysol_1/app.js
@@ -1,8 +1,16 @@
 'use strict';
 
-let color,
-  background,
-  size;
+// let color,
+//   background,
+//   size;
 
-const rate = 65.9,
-  markup = 0.1;
+// const rate = 65.9,
+//   markup = 0.1;
+
+let color;
+let background;
+let size;
+
+const rate = 65.9;
+const markup = 0.1;
+
