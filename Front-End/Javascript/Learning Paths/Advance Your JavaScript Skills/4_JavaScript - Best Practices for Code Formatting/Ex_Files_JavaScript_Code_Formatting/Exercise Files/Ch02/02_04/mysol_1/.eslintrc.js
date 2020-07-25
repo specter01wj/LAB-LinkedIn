df8291a455981e92ee6f895b581cdba13228ab4d
specter01wj <specter01wj@gmail.com>
2020-07-25 13:17:38 -0400

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/4_JavaScript - Best Practices for Code Formatting/Ex_Files_JavaScript_Code_Formatting/Exercise Files/Ch02/02_04/mysol_1/.eslintrc.js
+++ Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/4_JavaScript - Best Practices for Code Formatting/Ex_Files_JavaScript_Code_Formatting/Exercise Files/Ch02/02_04/mysol_1/.eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
     es6: true
   },
   rules: {
-
+    strict: ['error', 'global'],
+    'multiline-comment-style': ['error', 'starred-block'],
+    'spaced-comment': ['error', 'always'],
   },
 };
