b0859e446dbc7739f6c1405ab031998cad530c0b
specter01wj <specter01wj@gmail.com>
2020-07-24 23:39:03 -0400

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/5_JavaScript - Best Practices for Data/Ex_Files_JavaScript_BP_Data/Exercise Files/Ch05/05_02/mysol_1/app.js
+++ Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/5_JavaScript - Best Practices for Data/Ex_Files_JavaScript_BP_Data/Exercise Files/Ch05/05_02/mysol_1/app.js
@@ -7,13 +7,22 @@ const data = {
 };
 
 let alert = false;
-if ('1' === data.alert) {
+/* if ('1' === data.alert) {
   alert = 'winter storm';
 } else if ('2' === data.alert) {
   alert = 'high wind';
 } else if ('3' === data.alert) {
   alert = 'hurricane';
 } else if ('4' === data.alert) {
+  alert = 'heat advisory';
+}  */
+if (data.alert === '1') {
+  alert = 'winter storm';
+} else if (data.alert === '2') {
+  alert = 'high wind';
+} else if (data.alert === '3') {
+  alert = 'hurricane';
+} else if (data.alert === '4') {
   alert = 'heat advisory';
 } 
 
