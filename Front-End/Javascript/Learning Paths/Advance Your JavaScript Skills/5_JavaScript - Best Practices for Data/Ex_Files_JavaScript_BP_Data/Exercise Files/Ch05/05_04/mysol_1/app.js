85727893f0be49933020969119054e797928ebd1
specter01wj <specter01wj@gmail.com>
2020-07-24 23:44:59 -0400

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/5_JavaScript - Best Practices for Data/Ex_Files_JavaScript_BP_Data/Exercise Files/Ch05/05_04/mysol_1/app.js
+++ Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/5_JavaScript - Best Practices for Data/Ex_Files_JavaScript_BP_Data/Exercise Files/Ch05/05_04/mysol_1/app.js
@@ -4,6 +4,13 @@ const data = {
   warning: true,
   warningText: 'Solar flare may disrupt communications',
 };
-const warning = data.warning ? true : false;
+/* const warning = data.warning ? true : false;
 
-console.log(`Warning: ${(warning) ? data.warningText : 'No warning message at this time'}`);
+console.log(`Warning: ${(warning) ? data.warningText : 'No warning message at this time'}`); */
+
+const nullWarning = 'No warning message at this time';
+const warning = data.warning;
+const text = data.warningText;
+
+console.log(`Warning: ${(warning) ? text : nullWarning}`);
+
