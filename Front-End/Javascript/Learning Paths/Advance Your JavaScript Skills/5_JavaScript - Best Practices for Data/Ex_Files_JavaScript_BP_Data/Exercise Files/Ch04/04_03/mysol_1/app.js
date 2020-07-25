65a9d587e37beebbc81fbb3dea93a10fbf0e5157
specter01wj <specter01wj@gmail.com>
2020-07-24 23:11:39 -0400

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/5_JavaScript - Best Practices for Data/Ex_Files_JavaScript_BP_Data/Exercise Files/Ch04/04_03/mysol_1/app.js
+++ Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/5_JavaScript - Best Practices for Data/Ex_Files_JavaScript_BP_Data/Exercise Files/Ch04/04_03/mysol_1/app.js
@@ -5,14 +5,14 @@ const formData = ['flower', 0, undefined];
 formData.forEach((el) => {
   console.log(`\n${el}:`);
   console.table({
-    'new Boolean()': {
+    /* 'new Boolean()': {
       result: `${new Boolean(el)}`, 
       'data type': `${typeof new Boolean(el)}`,
     },
     'Boolean()': { 
       result: `${Boolean(el)}`, 
       'data type': `${typeof Boolean(el)}`,
-    },
+    }, */
     '!!': {
       result: `${!!el}`, 
       'data type': `${typeof !!el}`,
