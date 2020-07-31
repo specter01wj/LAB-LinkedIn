c1733ae1f74624c3e9b4533736c73781c3429591
specter01wj <specter01wj@gmail.com>
2020-07-31 12:39:25 -0400

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/4_JavaScript - Best Practices for Code Formatting/Ex_Files_JavaScript_Code_Formatting/Exercise Files/Ch04/04_10/mysol_1/script.js
+++ Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/4_JavaScript - Best Practices for Code Formatting/Ex_Files_JavaScript_Code_Formatting/Exercise Files/Ch04/04_10/mysol_1/script.js
@@ -3,22 +3,30 @@
 
 // App setup
 const departments = [
-  'produce' ,
-    'dairy' ,
-     'meat' ,
-   'bakery' ,
-'beverages' ,
- 'packaged' ,
+  'produce',
+  'dairy',
+  'meat',
+  'bakery',
+  'beverages',
+  'packaged',
 ];
 let cart = {};
 
-for( let i=0; i<departments.length; i+=1 ) 
-{
-  cart[ departments[ i ] ] = '';
+for (let i = 0; i < departments.length; i += 1) {
+  cart[departments[i]] = '';
 }
 
-console.log( cart );
+console.log(cart);
 
-
-
-const itemLookup=( barcode )=>{ fetch( `https://world.openfoodfacts.org/api/v0/product/${barcode}.json` ).then( ( response ) => { if ( response.ok ) { return response.json(); } else { throw response.status; }} ).then( ( data ) => renderProductInfo( data ) ).catch( ( error ) => console.log( 'Error: ', error ) ); };
+const itemLookup = (barcode) => { 
+  fetch(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json`)
+    .then((response) => { 
+      if (response.ok) { 
+        return response.json(); 
+      } else { 
+        throw response.status; 
+      } 
+    })
+    .then((data) => renderProductInfo(data))
+    .catch((error) => console.log('Error: ', error)); 
+  };
