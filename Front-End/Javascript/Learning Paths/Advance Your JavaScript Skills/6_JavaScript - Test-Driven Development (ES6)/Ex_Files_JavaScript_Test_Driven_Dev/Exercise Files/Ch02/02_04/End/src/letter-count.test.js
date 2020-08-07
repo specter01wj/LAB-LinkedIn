bcb9eb3e8503ea8cf9a2a40044567503b17f993e
specter01wj <specter01wj@gmail.com>
2020-08-06 22:59:00 -0400

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/6_JavaScript - Test-Driven Development (ES6)/Ex_Files_JavaScript_Test_Driven_Dev/Exercise Files/Ch02/02_04/End/src/letter-count.test.js
+++ Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/6_JavaScript - Test-Driven Development (ES6)/Ex_Files_JavaScript_Test_Driven_Dev/Exercise Files/Ch02/02_04/End/src/letter-count.test.js
@@ -4,6 +4,7 @@ import { getLetterCount } from './letter-count';
 describe('getLetterCount - basic functionality', () => {
     it('returns an empty object when passed an empty string', () => {
         const expected = {};
+        
         const actual = getLetterCount('');
         expect(actual).to.deep.equal(expected);
     })
