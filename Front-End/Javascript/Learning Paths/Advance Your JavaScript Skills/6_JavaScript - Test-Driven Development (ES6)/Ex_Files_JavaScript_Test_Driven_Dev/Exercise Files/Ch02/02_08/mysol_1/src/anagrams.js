22aba746aeff67c048e9dbb0ca62c3d8bb7abf98
Jin Wang <specter01wj@gmail.com>
2020-08-13 00:13:35 -0400

    06/04/2020 v1

---
+++ Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/6_JavaScript - Test-Driven Development (ES6)/Ex_Files_JavaScript_Test_Driven_Dev/Exercise Files/Ch02/02_08/mysol_1/src/anagrams.js
@@ -0,0 +1,34 @@
+/* import { isEqual } from 'lodash';
+import { getLetterCount } from './letter-count';
+
+export const formatString = string =>
+    string.toLowerCase().replace(/\s+/g, '');
+
+export const isAnagram = (string1, string2) => {
+    const string1LetterCount = getLetterCount(formatString(string1));
+    const string2LetterCount = getLetterCount(formatString(string2));
+
+    return isEqual(string1LetterCount, string2LetterCount);
+}; */
+
+
+export const isAnagram = (str1, str2) => {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let counter = new Array(26).fill(0);
+
+  for(let i = 0; i < str1.length; i++) {
+    counter[str1.charCodeAt(i) - 97]++;
+    counter[str2.charCodeAt(i) - 97]--;
+  }
+
+  for(let count of counter) {
+      if(count != 0) {
+          return false;
+      }
+  }
+
+  return true;
+};
+
