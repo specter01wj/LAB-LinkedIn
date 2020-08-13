1279f45871fabf30b58572ef5aae21f110a43bd1
Jin Wang <specter01wj@gmail.com>
2020-08-12 23:54:39 -0400

    06/04/2020 v1

--- Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/6_JavaScript - Test-Driven Development (ES6)/Ex_Files_JavaScript_Test_Driven_Dev/Exercise Files/Ch02/02_07/mysol_1/src/letter-count.test.js
+++ Front-End/Javascript/Learning Paths/Advance Your JavaScript Skills/6_JavaScript - Test-Driven Development (ES6)/Ex_Files_JavaScript_Test_Driven_Dev/Exercise Files/Ch02/02_07/mysol_1/src/letter-count.test.js
@@ -11,11 +11,6 @@ describe('getLetterCount - basic functionality', () => {
     it('return the correct letter count for a word with only one of each letter', () => {
         const expected = { c: 1, a: 1, t: 1 };
         const actual = getLetterCount('cat');
-
-
-
-
-        
         expect(actual).to.deep.equal(expected);
     });
 
