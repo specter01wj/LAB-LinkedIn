65db3357d545afdbf0c5a5e5da51783882362b46
Jin Wang <specter01wj@gmail.com>
2022-12-16 18:43:55 -0500

    v1

---
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch01/01_07/mysol_1/example.js
@@ -0,0 +1,16 @@
+const x = 5;
+// x = 6;
+
+const numbers = [1,2,3,4,5];
+numbers[0] = 100;
+numbers.reverse();
+
+console.log(numbers);
+
+const person = {
+    name: 'John Doe',
+    age: 34,
+};
+person.name = 'Bob';
+
+console.log(person);
