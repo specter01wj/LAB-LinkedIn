b5c0e81560fd817b1b8b1d551665f8c7355c5326
Jin Wang <specter01wj@gmail.com>
2023-01-02 23:52:04 -0500

    v10

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_04/mysol_1/ex_02-04.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_04/mysol_1/ex_02-04.js
@@ -4,8 +4,8 @@ const subtract = (x, y) => x - y;
 const combine2and3 = func =>
     func(2, 3);
 
-combine2and3(add); // -> 2 + 3
-combine2and3((x, y) => x + y);
-combine2and3(subtract); // -> 2 - 3
+console.log(combine2and3(add)); // -> 2 + 3
+console.log(combine2and3((x, y) => x + y));
+console.log(combine2and3(subtract)); // -> 2 - 3
 
-combine2and3(Math.max) // -> 3
+console.log(combine2and3(Math.max)); // -> 3
