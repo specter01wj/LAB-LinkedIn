4503430e09018e6d1f62fdf5cd8339661e4f2221
Jin Wang <specter01wj@gmail.com>
2023-01-03 01:51:55 -0500

    v12

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_05/mysol_1/ex_02-05.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch02/02_05/mysol_1/ex_02-05.js
@@ -1,7 +1,17 @@
+const createMultiplier_1 = function() {
+  return function() {
+    console.log('hello');
+  }
+};
+
 const createMultiplier = y => x => x * y;
+
+// const double = x => x * 2;
+// const triple = x => x * 3;
+// const quadruple = x => x * 4;
 
 const double = createMultiplier(2);
 const triple = createMultiplier(3);
 const quadruple = createMultiplier(4);
 
-double(3);
+console.log(double(3));
