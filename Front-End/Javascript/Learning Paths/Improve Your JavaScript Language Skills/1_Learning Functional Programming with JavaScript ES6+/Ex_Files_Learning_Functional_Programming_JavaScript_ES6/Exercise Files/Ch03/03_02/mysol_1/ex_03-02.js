459c2abea24fe022a353aeee89a3a21da0af6352
Jin Wang <specter01wj@gmail.com>
2023-01-04 01:35:02 -0500

    v1

---
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch03/03_02/mysol_1/ex_03-02.js
@@ -0,0 +1,37 @@
+const person = {
+    name: 'Jimmy Smith',
+    age: 40,
+    hairColor: 'brown',
+    eyeColor: 'blue',
+};
+
+const careerData = {
+    name: 'Bob Smith',
+    title: 'developer',
+    company: 'ABC Software',
+};
+
+const personWithCareerData = {
+  ...person,
+  ...careerData,
+};
+
+console.log(personWithCareerData);
+
+const updates = {
+    name: 'James Smith',
+};
+
+const updatedPerson = {
+    ...person,
+    ...updates,
+};
+
+const numbers = [1, 2, 3, 4, 5];
+const newNumbers = [
+    0,
+    ...numbers,
+    6,
+];
+
+console.log(newNumbers);
