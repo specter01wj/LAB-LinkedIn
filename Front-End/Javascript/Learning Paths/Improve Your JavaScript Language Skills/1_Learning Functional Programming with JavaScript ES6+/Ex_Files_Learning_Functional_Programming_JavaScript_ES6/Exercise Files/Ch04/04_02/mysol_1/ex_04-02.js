56c4116f2159e849146bcdc2367c16716f43cddc
Jin Wang <specter01wj@gmail.com>
2023-01-05 18:41:20 -0500

    v2

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch04/04_02/mysol_1/ex_04-02.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch04/04_02/mysol_1/ex_04-02.js
@@ -7,3 +7,12 @@ const addPartial = x =>
 const sum = addPartial(5)(6)(7);
 
 console.log(sum);
+
+
+const addPartial_2 = x =>
+    (y, z)=> add(x, y, z);
+
+const add5 = addPartial_2(50);
+const sum5 = add5(60, 70);
+
+console.log(sum5);
