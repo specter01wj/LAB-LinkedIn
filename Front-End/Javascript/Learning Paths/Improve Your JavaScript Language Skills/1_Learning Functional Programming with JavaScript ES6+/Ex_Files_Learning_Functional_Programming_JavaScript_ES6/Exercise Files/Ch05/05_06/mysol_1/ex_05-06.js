fa68e3bafd26a99a06d73ad7d83a9ef68144a659
Jin Wang <specter01wj@gmail.com>
2023-01-06 00:28:24 -0500

    v2

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch05/05_06/mysol_1/ex_05-06.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch05/05_06/mysol_1/ex_05-06.js
@@ -1,19 +1,39 @@
 const currentInputValues = {
-    firstName: '', // Must be at least 2 characters
-    lastName: '', // Must be at least 2 characters
-    zipCode: '', // Must be exactly 5 characters
-    state: '', // Must be exactly 2 characters
+    firstName: 'J', // Must be at least 2 characters
+    lastName: 'W', // Must be at least 2 characters
+    zipCode: '2086', // Must be exactly 5 characters
+    state: 'MDVA', // Must be exactly 2 characters
 }
 
 const inputCriteria = {
-    firstName: [],
-    lastName: [],
-    zipCode: [],
-    state: [],
+    firstName: [
+      value => value.length >= 2
+            ? ''
+            : 'First name must be at least 2 characters',
+    ],
+    lastName: [
+      value => value.length >= 2
+            ? ''
+            : 'Last name must be at least 2 characters',
+    ],
+    zipCode: [
+      value => value.length === 5
+            ? ''
+            : 'Zip must be exactly 5 characters long',
+    ],
+    state: [
+      value => value.length === 2
+            ? ''
+            : 'State must be exactly 2 characters long',
+    ],
 };
 
 const getErrorMessages = (inputs, criteria) => {
-
+  return Object.keys(inputs).reduce((acc, fieldName) => [
+      ...acc,
+      ...criteria[fieldName].map(test =>
+          test(inputs[fieldName])),
+  ], []).filter(message => message);
 }
 
 console.log(getErrorMessages(currentInputValues, inputCriteria));
