7edad0285c375a72878cdc7133cee7bcda59af3d
Jin Wang <specter01wj@gmail.com>
2023-01-05 19:15:32 -0500

    v7

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch04/04_04/mysol_1/ex_04-04.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch04/04_04/mysol_1/ex_04-04.js
@@ -9,5 +9,7 @@ console.log(add.call(null, 1, 2, 3));
 console.log(add.apply(null, [1, 2, 3]));
 
 const args = [1, 2, 3];
+console.log(add(...args));
+
 const add1 = add.bind(null, 1);
 console.log(add1(2, 3));
