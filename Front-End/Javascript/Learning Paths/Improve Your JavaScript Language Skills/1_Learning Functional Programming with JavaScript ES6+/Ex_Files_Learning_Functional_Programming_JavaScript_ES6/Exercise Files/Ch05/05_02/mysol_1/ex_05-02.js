eb0480e9a890205820a477e23546cbd399ff268a
Jin Wang <specter01wj@gmail.com>
2023-01-05 19:35:05 -0500

    v9

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch05/05_02/mysol_1/ex_05-02.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/1_Learning Functional Programming with JavaScript ES6+/Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch05/05_02/mysol_1/ex_05-02.js
@@ -7,10 +7,22 @@ const electionVotes = [
 ];
 
 const tallyVotes = votes => {
-    // Your code here
+    return votes.reduce((acc, item) => ({
+      ...acc,
+      [item]: acc[item] ? ++acc[item] : 1,
+    }), {});
+};
+
+const tallyVotes_2 = votes => {
+    let nameMap = {};
+    votes.map( (item) => {
+      nameMap[item] = nameMap[item] ? ++nameMap[item] : 1;
+    });
+    return nameMap;
 };
 
 console.log(tallyVotes(electionVotes));
+console.log(tallyVotes_2(electionVotes));
 
 /* Expected Output (something like this):
     {
