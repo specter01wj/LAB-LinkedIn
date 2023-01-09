eef0510562935b76c7f9ada1576464de7a0f8178
Jin Wang <specter01wj@gmail.com>
2023-01-09 01:25:07 -0500

    v4

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/2_JavaScript - Recursion/Ch02/02_03e/join_string.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/2_JavaScript - Recursion/Ch02/02_03e/join_string.js
@@ -1,16 +1,16 @@
-function join(content, seperatedValue){
-	function padContent(index, currentString){
-		currentString += content[index]
-		if(index === content.length - 1){
-			return currentString
-		} else {
-			console.log(currentString, index, content.length)
+function join(content, seperatedValue) {
+  function padContent(index, currentString) {
+    currentString += content[index];
+    if (index === content.length - 1) {
+      return currentString;
+    } else {
+      console.log(currentString, index, content.length);
 
-			return padContent(index + 1, currentString + seperatedValue)
-		}
-	}
-	return padContent(0, '')
+      return padContent(index + 1, currentString + seperatedValue);
+    }
+  }
+  return padContent(0, "");
 }
 
-console.log( join(['p','h', 'o', 'n', 'e'], '1') )
-console.log( join('shinbain', '1') )
+console.log(join(["p", "h", "o", "n", "e"], "1"));
+console.log(join("shinbain", "1"));
