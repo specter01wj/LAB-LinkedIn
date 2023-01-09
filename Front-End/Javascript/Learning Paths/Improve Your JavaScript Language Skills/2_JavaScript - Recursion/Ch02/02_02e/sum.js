52ebd34593914af99ad4a466fe444ddf763604f1
Jin Wang <specter01wj@gmail.com>
2023-01-09 01:24:09 -0500

    v3

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/2_JavaScript - Recursion/Ch02/02_02e/sum.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/2_JavaScript - Recursion/Ch02/02_02e/sum.js
@@ -1,13 +1,12 @@
-function sum( start, end ){
-    function addNext( newStart, currentSum ){
-        if ( newStart > end ){
-            return currentSum
-        }
-        else {
-            return addNext(newStart + 1, newStart + currentSum)
-        }
+function sum(start, end) {
+  function addNext(newStart, currentSum) {
+    if (newStart > end) {
+      return currentSum;
+    } else {
+      return addNext(newStart + 1, newStart + currentSum);
     }
-    return addNext( start, 0 )
+  }
+  return addNext(start, 0);
 }
 
-console.log( sum( 2, 5) )
+console.log(sum(2, 5));
