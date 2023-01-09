1d9485e3873f69e0a0101c29c0f388daabee193d
Jin Wang <specter01wj@gmail.com>
2023-01-08 23:57:56 -0500

    v12

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/2_JavaScript - Recursion/Ch02/02_01b/head_factorial.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/2_JavaScript - Recursion/Ch02/02_01b/head_factorial.js
@@ -1,13 +1,11 @@
-function headFactorial(index){
- if (index === 0)
-    return 1
- else{
-     console.trace()
-     return index * headFactorial(index -1)       
- }
+function headFactorial(index) {
+  if (index === 0) return 1;
+  else {
+    console.trace();
+    return index * headFactorial(index - 1);
+  }
 }
 
-
-console.time("headFactorial") // start timer
-console.log( headFactorial(50) )
-console.timeEnd("headFactorial") // stop timer
+console.time("headFactorial"); // start timer
+console.log(headFactorial(50));
+console.timeEnd("headFactorial"); // stop timer
