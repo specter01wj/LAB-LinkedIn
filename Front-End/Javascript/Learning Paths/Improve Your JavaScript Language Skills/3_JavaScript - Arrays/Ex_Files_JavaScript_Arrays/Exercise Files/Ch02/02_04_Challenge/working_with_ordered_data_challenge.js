3757cc30a14648749b7518c75871194aa13a70b6
Jin Wang <specter01wj@gmail.com>
2023-01-11 00:45:46 -0500

    v6

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch02/02_04_Challenge/working_with_ordered_data_challenge.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch02/02_04_Challenge/working_with_ordered_data_challenge.js
@@ -9,9 +9,13 @@ const students = [
 //Challenge Question #1
 //Sort the array of students based on their grade,
 //in descending order(largest to smallest).
+students.sort((a, b) => b.grade - a.grade);
+console.log(students);
 
 //Challenge Question #2
 //After sorting the array, reverse the order of the array.
+console.log(students.reverse());
 
 //Challenge Question #3
 //Find a student in the array who has a grade over 90.
+console.log(students.find((elem) => elem.grade > 90));
