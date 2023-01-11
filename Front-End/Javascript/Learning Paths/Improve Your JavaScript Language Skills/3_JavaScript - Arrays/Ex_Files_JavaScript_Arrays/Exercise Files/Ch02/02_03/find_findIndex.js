2ba7dcc669b1501eb229d4a278c26e29b91b75bb
Jin Wang <specter01wj@gmail.com>
2023-01-11 00:34:47 -0500

    v3

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch02/02_03/find_findIndex.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch02/02_03/find_findIndex.js
@@ -1,5 +1,7 @@
-const grades = [99, 83, 87, 91, 77];
+const grades = [99, 83, 65, 87, 91, 77];
 
 //Find the first grade that is less than 80.
+console.log(grades.find((elem) => elem < 80));
 
 //Find the index of the first grade that is over 80.
+console.log(grades.findIndex((elem) => elem > 80));
