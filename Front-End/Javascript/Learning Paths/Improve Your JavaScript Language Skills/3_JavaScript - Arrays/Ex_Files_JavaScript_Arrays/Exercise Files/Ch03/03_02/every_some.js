d6d68f44edff17eaf7d4e7c0b1f2ede02f1ffe3a
Jin Wang <specter01wj@gmail.com>
2023-01-11 01:07:57 -0500

    v10

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch03/03_02/every_some.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch03/03_02/every_some.js
@@ -10,5 +10,7 @@ const states = [
 //Use this handy string method to find the solution: startsWith("New")
 
 //Do some of the states in the array start with "New"?
+console.log(states.some((state) => state.startsWith('New')));
 
 //Does every state in the array start with "New"?
+console.log(states.every((state) => state.startsWith('New')));
