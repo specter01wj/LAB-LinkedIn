7b56b88993be88a3e959253317365e963f18d191
Jin Wang <specter01wj@gmail.com>
2023-01-11 01:21:05 -0500

    v12

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch03/03_03_Challenge/evaluating_data_for_a_single_value_challenge.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch03/03_03_Challenge/evaluating_data_for_a_single_value_challenge.js
@@ -3,9 +3,12 @@ const bowlingScores = [154, 204, 300, 184, 286];
 
 //Challenge Question #1
 //Does the array of bowling scores include a 300?
+console.log(bowlingScores.includes(300));
 
 //Challenge Question #2
 //Are some of the scores under 150?
+console.log(bowlingScores.some((score) => score < 150));
 
 //Challenge Question #3
 //Is every score an even number?
+console.log(bowlingScores.every((score) => score % 2 === 0));
