ccf7ca5e4dff836717a5420a99d01e30e3c738f0
Jin Wang <specter01wj@gmail.com>
2023-01-10 23:50:07 -0500

    v6

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch02/02_01/sort.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch02/02_01/sort.js
@@ -1,6 +1,8 @@
 //Sort the following array alphabetically
 const pets = ['dog', 'cat', 'rabbit', 'hamster'];
 
+console.log(pets.sort());
+
 const grades = [88, 94, 72, 99, 53, 77];
 //Sort the following array from highest to lowest
 
