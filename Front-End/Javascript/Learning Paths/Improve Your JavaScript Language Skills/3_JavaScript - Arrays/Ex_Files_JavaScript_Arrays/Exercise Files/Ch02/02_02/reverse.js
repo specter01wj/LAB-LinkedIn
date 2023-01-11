0048fac7577e4055efea8d50fd555eb71e169834
Jin Wang <specter01wj@gmail.com>
2023-01-11 00:24:28 -0500

    v2

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch02/02_02/reverse.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch02/02_02/reverse.js
@@ -5,3 +5,6 @@ console.log(condiments.reverse());
 const employees = ['Adams', 'Carter', 'Davis', 'Fraklin', 'Zappa'];
 // Use reverse to change the order of this array,
 // but keep the original array intact.
+
+console.log([...employees].reverse());
+console.log(employees);
