b76558802d4254895c1970b06b9eeeb5ae169217
Jin Wang <specter01wj@gmail.com>
2023-01-12 01:24:54 -0500

    v2

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch04/04_02/unshift_shift.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch04/04_02/unshift_shift.js
@@ -6,3 +6,5 @@ console.log(ages);
 const holidays = ['Labor Day', 'Christmas', 'Hanakkuh', 'Independence Day'];
 //How do we remove the fist item from the holidays array?
 //Store the element that is removed in a variable.
+console.log(holidays.shift());
+console.log(holidays);
