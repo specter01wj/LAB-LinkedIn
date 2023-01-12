aaddd19abb979897651b8315ee63aae926cca199
Jin Wang <specter01wj@gmail.com>
2023-01-11 19:33:35 -0500

    v5

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch04/04_01/push_pop.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch04/04_01/push_pop.js
@@ -1,6 +1,11 @@
 const drinks = ['coffee', 'tea', 'soda', 'water'];
 //How do we add "wine" to the array of drinks?
+drinks.push('wine');
+console.log(drinks);
 
 const seasons = ['spring', 'summer', 'fall', 'winter'];
 //How do we remove the last season from the seasons array?
 //Store the element that is removed in a variable.
+let poppedValue = seasons.pop();
+console.log(poppedValue);
+console.log(seasons);
