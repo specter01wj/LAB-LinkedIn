53bc4adaa7aed30d2d7a8a407f199b17b5418b62
Jin Wang <specter01wj@gmail.com>
2023-01-12 01:22:16 -0500

    v1

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch04/04_02/unshift_shift.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch04/04_02/unshift_shift.js
@@ -1,5 +1,7 @@
 const ages = [18, 33, 71, 29, 65, 62];
 //How do we add 36 to the array of ages?
+console.log(ages.unshift(36));
+console.log(ages);
 
 const holidays = ['Labor Day', 'Christmas', 'Hanakkuh', 'Independence Day'];
 //How do we remove the fist item from the holidays array?
