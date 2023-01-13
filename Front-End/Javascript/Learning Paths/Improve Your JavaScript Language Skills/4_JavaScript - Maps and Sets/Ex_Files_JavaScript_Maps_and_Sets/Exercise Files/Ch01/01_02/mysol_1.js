e5d581fbc546c124a4812a979efcdae3d5b5838e
Jin Wang <specter01wj@gmail.com>
2023-01-13 17:18:46 -0500

    v5

---
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/4_JavaScript - Maps and Sets/Ex_Files_JavaScript_Maps_and_Sets/Exercise Files/Ch01/01_02/mysol_1.js
@@ -0,0 +1,13 @@
+//Saturday To Do List
+//8am: Walk the dogs
+//12pm: Lunch
+//3pm: Watch a movie
+
+//Create a new Map() and add each to-do as a key-value pair.
+let saturday = new Map();
+
+saturday.set(8, 'Walk the dogs');
+saturday.set(12, 'Lunch');
+saturday.set(15, 'Watch a movie');
+
+console.log(saturday);
