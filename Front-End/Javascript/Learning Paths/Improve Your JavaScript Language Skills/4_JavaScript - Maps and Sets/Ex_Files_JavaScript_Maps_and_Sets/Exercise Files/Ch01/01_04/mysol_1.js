a9cbce4e4a008bba73c62f050dda0065c70771ae
Jin Wang <specter01wj@gmail.com>
2023-01-13 18:12:19 -0500

    v6

---
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/4_JavaScript - Maps and Sets/Ex_Files_JavaScript_Maps_and_Sets/Exercise Files/Ch01/01_04/mysol_1.js
@@ -0,0 +1,17 @@
+//Saturday To Do List
+//8am: Walk the dogs
+//12pm: Lunch
+//3pm: Watch a movie
+
+const saturday = new Map();
+
+saturday.set(8, 'walk the dogs');
+saturday.set(12, 'lunch');
+saturday.set(3, 'watch a movie');
+
+//Does our Map have a value associated with 4?
+console.log(saturday.has(4));
+
+//Does our Map have a value associated with 8?
+console.log(saturday.has(8));
+
