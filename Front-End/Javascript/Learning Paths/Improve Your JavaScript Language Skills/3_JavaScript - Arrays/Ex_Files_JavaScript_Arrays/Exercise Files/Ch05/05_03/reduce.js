a55416e394b3249fa5495c34bd31cfcd230ddfbb
Jin Wang <specter01wj@gmail.com>
2023-01-13 01:30:47 -0500

    v3

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch05/05_03/reduce.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/3_JavaScript - Arrays/Ex_Files_JavaScript_Arrays/Exercise Files/Ch05/05_03/reduce.js
@@ -1,3 +1,8 @@
 const bills = [1200, 29.99, 65.93, 17.32, 44.76];
 
 //Determine how much the total bills are using the reduce method
+let totalBills = bills.reduce((acc, curr) => {
+  return acc + curr;
+}, 0);
+
+console.log(totalBills);
