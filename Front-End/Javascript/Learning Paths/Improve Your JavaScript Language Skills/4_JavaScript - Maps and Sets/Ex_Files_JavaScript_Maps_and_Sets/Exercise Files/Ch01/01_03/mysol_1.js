e132cc69b68902f95033d8753def3e60626b5d59
Jin Wang <specter01wj@gmail.com>
2023-01-13 17:28:30 -0500

    v7

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/4_JavaScript - Maps and Sets/Ex_Files_JavaScript_Maps_and_Sets/Exercise Files/Ch01/01_03/mysol_1.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/4_JavaScript - Maps and Sets/Ex_Files_JavaScript_Maps_and_Sets/Exercise Files/Ch01/01_03/mysol_1.js
@@ -8,6 +8,11 @@ const saturday = new Map();
 saturday.set(8, 'walk the dogs');
 saturday.set(12, 'lunch');
 saturday.set(3, 'watch a movie');
+saturday.set('7pm', 'dinner');
 
 //How do we get the value associated with the 12 key?
+console.log(saturday.get(12));
 //How do we get the value associated with the 9 key?
+console.log(saturday.get(9));
+
+console.log(saturday.get('7pm'));
