9ab69aa60a3af23b8b8a31ad781eb2cf3ae56ae9
Jin Wang <specter01wj@gmail.com>
2023-01-13 19:10:24 -0500

    v10

---
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/4_JavaScript - Maps and Sets/Ex_Files_JavaScript_Maps_and_Sets/Exercise Files/Ch01/01_07/mysol_1.js
@@ -0,0 +1,22 @@
+//Saturday To Do List
+//8am: Walk the dogs
+//12pm: Lunch
+//3pm: Watch a movie
+
+const saturday = new Map();
+
+saturday.set(8, 'walk the dogs');
+saturday.set(12, 'lunch');
+saturday.set(3, 'watch a movie');
+
+//What is the first key in your saturday Map object?
+console.log(saturday.keys());
+console.log(saturday.keys().next());
+//What is the second value in your saturday Map object?
+console.log(saturday.values());
+console.log(saturday.values().next());
+
+let saturdayValue = saturday.values();
+saturdayValue.next();
+console.log(saturdayValue.next());
+
