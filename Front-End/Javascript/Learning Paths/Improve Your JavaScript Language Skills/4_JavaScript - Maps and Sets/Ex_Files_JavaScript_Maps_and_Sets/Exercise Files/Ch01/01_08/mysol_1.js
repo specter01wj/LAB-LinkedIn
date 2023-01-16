8f8b13221017a4bd2ee68a3e0b77603d98c05764
Jin Wang <specter01wj@gmail.com>
2023-01-16 01:06:03 -0500

    v1

---
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/4_JavaScript - Maps and Sets/Ex_Files_JavaScript_Maps_and_Sets/Exercise Files/Ch01/01_08/mysol_1.js
@@ -0,0 +1,18 @@
+//Saturday To Do List
+//8am: Walk the dogs
+//12pm: Lunch
+//3pm: Watch a movie
+
+const saturday = new Map();
+
+saturday.set(8, 'walk the dogs');
+saturday.set(12, 'lunch');
+saturday.set(3, 'watch a movie');
+
+//What is the third set of entries in your saturday Map object?
+let saturdayEntries = saturday.entries();
+console.log(saturdayEntries);
+
+saturdayEntries.next();
+saturdayEntries.next();
+console.log(saturdayEntries.next());
