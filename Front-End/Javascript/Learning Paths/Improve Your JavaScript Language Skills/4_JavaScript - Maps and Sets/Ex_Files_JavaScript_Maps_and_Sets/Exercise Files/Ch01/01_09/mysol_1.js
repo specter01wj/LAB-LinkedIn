4b9747dc22492d5f639019ae91d54e7dcbad0545
Jin Wang <specter01wj@gmail.com>
2023-01-16 01:15:19 -0500

    v2

---
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/4_JavaScript - Maps and Sets/Ex_Files_JavaScript_Maps_and_Sets/Exercise Files/Ch01/01_09/mysol_1.js
@@ -0,0 +1,19 @@
+//Saturday To Do List
+//8am: Walk the dogs
+//12pm: Lunch
+//3pm: Watch a movie
+
+const saturday = new Map();
+
+saturday.set(8, 'walk the dogs');
+saturday.set(12, 'lunch');
+saturday.set(3, 'watch a movie');
+
+//Using the forEach method, if a key is equal to 12, 
+//console log the value of the key.
+saturday.forEach((val, key) => {
+  if (key === 12) {
+    console.log(`${val}`);
+  }
+});
+
