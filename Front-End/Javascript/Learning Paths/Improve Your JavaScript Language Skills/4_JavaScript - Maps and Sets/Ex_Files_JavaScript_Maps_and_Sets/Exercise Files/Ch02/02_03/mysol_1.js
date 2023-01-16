12e6672fad9d54cbec5a8882bd2fa9fbe77221aa
Jin Wang <specter01wj@gmail.com>
2023-01-16 13:03:57 -0500

    v2

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/4_JavaScript - Maps and Sets/Ex_Files_JavaScript_Maps_and_Sets/Exercise Files/Ch02/02_03/mysol_1.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/4_JavaScript - Maps and Sets/Ex_Files_JavaScript_Maps_and_Sets/Exercise Files/Ch02/02_03/mysol_1.js
@@ -6,8 +6,16 @@ const losAngeles = {city: "Los Angeles"};
 //Create a WeakMap using the provided variables as keys.
 //Set the value to whatever term you think of when you hear
 //those cities. The value can be anything. Hint: use set()
+let cities = new WeakMap();
+cities.set(newYork, "Big Apple");
+cities.set(nola, "Mardi Gras");
+cities.set(chicago, "Windy City");
+cities.set(losAngeles, "Hollywood");
+
+console.log(cities);
 
 //Get the value associated with the key: nola.
+console.log(cities.get(nola));
 
 //Get the value associated with the key: dallas.
 const dallas = {city: "Dallas"};
