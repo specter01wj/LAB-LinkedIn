905d77f844ec43435e074ab92f42fb1163becce8
Jin Wang <specter01wj@gmail.com>
2023-01-17 18:02:19 -0500

    v1

---
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/4_JavaScript - Maps and Sets/Ex_Files_JavaScript_Maps_and_Sets/Exercise Files/Ch03/03_02/mysol_1.js
@@ -0,0 +1,18 @@
+//Ice Cream Flavor Suggestions
+//chocolate
+//vanilla
+//coffee
+//coffee
+//strawberry
+//vanilla
+
+//Using the list of ice cream flavors above create a new Set.
+let iceCream = new Set();
+iceCream.add('chocolate')
+.add('vanilla')
+.add('coffee')
+.add('coffee')
+.add('strawberry')
+.add('vanilla');
+
+console.log(iceCream);
