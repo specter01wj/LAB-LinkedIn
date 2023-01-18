81f5912766b13fe46d7628448f56242cfd01726a
Jin Wang <specter01wj@gmail.com>
2023-01-18 00:24:16 -0500

    v6

---
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/4_JavaScript - Maps and Sets/Ex_Files_JavaScript_Maps_and_Sets/Exercise Files/Ch03/03_07/mysol_1.js
@@ -0,0 +1,19 @@
+//Ice Cream Flavor Suggestions
+//chocolate
+//vanilla
+//coffee
+//coffee
+//strawberry
+//vanilla
+
+const iceCream = new Set();
+iceCream.add('chocolate')
+.add('vanilla')
+.add('coffee')
+.add('coffee')
+.add('strawberry')
+.add('vanilla');
+
+//What is the first entry in our iceCream set?
+let iceCreamEntries = iceCream.entries();
+console.log(iceCreamEntries.next());
