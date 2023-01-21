a8e669e060852a3b0fb5405c1a0e775f82fd0684
Jin Wang <specter01wj@gmail.com>
2023-01-20 23:35:54 -0500

    v13

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/5_JavaScript - Async/Ex_Files_JavaScript_Async_2021/Exercise Files/Ch04/04_02/mysol_1/_scripts/filters.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/5_JavaScript - Async/Ex_Files_JavaScript_Async_2021/Exercise Files/Ch04/04_02/mysol_1/_scripts/filters.js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const outputCanvas = document.querySelector('#output');
   const ctx = outputCanvas.getContext('2d');
   let selectedFilter = '';
+  const worker = new Worker('_scripts/filter-worker.js');
 
   const getImageData = (image) => {
     const tempCanvas = document.createElement('canvas');
@@ -26,65 +27,65 @@ document.addEventListener('DOMContentLoaded', function() {
     return imageDataObj;
   };
 
-  const delay = (ms) => {
-    const startPoint = new Date().getTime()
-    while (new Date().getTime() - startPoint <= ms) {/* wait */}
-  };
+  // const delay = (ms) => {
+  //   const startPoint = new Date().getTime()
+  //   while (new Date().getTime() - startPoint <= ms) {/* wait */}
+  // };
 
-  const grayscaleImage = (pixels) => {
-    var d = pixels.data;
-    for (var i = 0; i < d.length; i += 4) {
-      var r = d[i];
-      var g = d[i + 1];
-      var b = d[i + 2];
-      // CIE luminance for the RGB
-      // The human eye is bad at seeing red and blue, so we de-emphasize them.
-      var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-      d[i] = d[i + 1] = d[i + 2] = v
-    }
-    delay(1000);
-    return pixels;
-  };
+  // const grayscaleImage = (pixels) => {
+  //   var d = pixels.data;
+  //   for (var i = 0; i < d.length; i += 4) {
+  //     var r = d[i];
+  //     var g = d[i + 1];
+  //     var b = d[i + 2];
+  //     // CIE luminance for the RGB
+  //     // The human eye is bad at seeing red and blue, so we de-emphasize them.
+  //     var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  //     d[i] = d[i + 1] = d[i + 2] = v
+  //   }
+  //   delay(1000);
+  //   return pixels;
+  // };
 
-  const brightnessImage = (adjustment, pixels) => {
-    var d = pixels.data;
-    for (var i = 0; i < d.length; i += 4) {
-      d[i] += adjustment;
-      d[i+1] += adjustment;
-      d[i+2] += adjustment;
-    }
-    delay(1000);
-    return pixels;
-  };
+  // const brightnessImage = (adjustment, pixels) => {
+  //   var d = pixels.data;
+  //   for (var i = 0; i < d.length; i += 4) {
+  //     d[i] += adjustment;
+  //     d[i+1] += adjustment;
+  //     d[i+2] += adjustment;
+  //   }
+  //   delay(1000);
+  //   return pixels;
+  // };
 
-  const thresholdImage = (level, pixelData) => {
-    const thresholdLevel = level;    // 0-255
-    var d = pixelData.data;
-    for (var i = 0; i < d.length; i += 4) {
-      var r = d[i];
-      var g = d[i + 1];
-      var b = d[i + 2];
-      var v = (0.2126 * r + 0.7152 * g + 0.0722 * b >= thresholdLevel) ? 255 : 0;
-      d[i] = d[i + 1] = d[i + 2] = v
-    }
-    delay(1000);
-    return pixelData;
-  };
+  // const thresholdImage = (level, pixelData) => {
+  //   const thresholdLevel = level;    // 0-255
+  //   var d = pixelData.data;
+  //   for (var i = 0; i < d.length; i += 4) {
+  //     var r = d[i];
+  //     var g = d[i + 1];
+  //     var b = d[i + 2];
+  //     var v = (0.2126 * r + 0.7152 * g + 0.0722 * b >= thresholdLevel) ? 255 : 0;
+  //     d[i] = d[i + 1] = d[i + 2] = v
+  //   }
+  //   delay(1000);
+  //   return pixelData;
+  // };
 
-  const applyFilter = (filter, level, image) => {
-    const startTime = new Date().getTime();
-    let results;
-    if (filter === 'brightness') {
-      results = brightnessImage(level, image);
-    } else if (filter === 'threshold') {
-      results = thresholdImage(level, image);
-    } else if (filter === 'grayscale') {
-      results = grayscaleImage(image);
-    }
-    const totalTime = new Date().getTime() - startTime;
-    console.log("Filtering: %d ms", totalTime);
-    return results;
-  };
+  // const applyFilter = (filter, level, image) => {
+  //   const startTime = new Date().getTime();
+  //   let results;
+  //   if (filter === 'brightness') {
+  //     results = brightnessImage(level, image);
+  //   } else if (filter === 'threshold') {
+  //     results = thresholdImage(level, image);
+  //   } else if (filter === 'grayscale') {
+  //     results = grayscaleImage(image);
+  //   }
+  //   const totalTime = new Date().getTime() - startTime;
+  //   console.log("Filtering: %d ms", totalTime);
+  //   return results;
+  // };
 
   const resetCanvas = () => {
     const imageDataObj = getImageData(image);
