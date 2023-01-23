c71f07d6c0d689c99230d497f812041586a4b839
Jin Wang <specter01wj@gmail.com>
2023-01-23 13:11:27 -0500

    v3

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/6_JavaScript - Patterns/Ex_Files_JavaScript_Patterns/Exercise Files/CH2/02_03/mysol_1/index.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/6_JavaScript - Patterns/Ex_Files_JavaScript_Patterns/Exercise Files/CH2/02_03/mysol_1/index.js
@@ -6,6 +6,15 @@ class Car {
     }
 }
 
+class SUV extends Car {
+  constructor(doors, engine, color, wheels) {
+    super(doors, engine, color);
+    this.wheels = wheels;
+  }
+}
+
 const civic = new Car(4, 'V6', 'grey');
+const cx5 = new SUV(4, "V8", 'red', 6);
 
 console.log(civic);
+console.log(cx5);
