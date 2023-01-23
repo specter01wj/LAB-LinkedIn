c106e92858be21f0f9c8378805c85f47245ae464
Jin Wang <specter01wj@gmail.com>
2023-01-23 18:26:35 -0500

    v12

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/7_JavaScript - The Tricky Bits/Ex_Files_JS_the_Tricky_Bits/Exercise Files/Ch01/01/mysol_1/main.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/7_JavaScript - The Tricky Bits/Ex_Files_JS_the_Tricky_Bits/Exercise Files/Ch01/01/mysol_1/main.js
@@ -12,9 +12,24 @@
         "Cécile McLorin Salvant",
     ];
 
+    console.log('1st------');
     for (let member of band) {
       console.log(member);
     }
+
+    console.log('2nd------');
+    band.forEach((member) => {
+      console.log("Introducing…", member);
+    })
+
+    console.log('3rd------');
+    band.every(function (member) {
+        console.log("Introducing…", member);
+        if (member === "Ingrid Jensen") {
+            return false;
+        }
+        return true;
+    });
 
     // Nested array
     var bands = [
@@ -37,10 +52,14 @@
         ["Chick Corea", "Miroslav Vitous", "Roy Haynes"],
     ];
 
+    console.log('4th------');
     for (let member1 of bands) {
       for (let member2 of member1) {
         console.log(member2);
       }
     }
+
+
+
 
 })();
