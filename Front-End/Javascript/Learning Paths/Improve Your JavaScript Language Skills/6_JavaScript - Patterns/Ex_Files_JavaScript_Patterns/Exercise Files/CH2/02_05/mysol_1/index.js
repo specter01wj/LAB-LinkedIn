8e7b6acc5a0e775201f89f1a1e2f92c8beb8bd18
Jin Wang <specter01wj@gmail.com>
2023-01-23 17:32:18 -0500

    v10

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/6_JavaScript - Patterns/Ex_Files_JavaScript_Patterns/Exercise Files/CH2/02_05/mysol_1/index.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/6_JavaScript - Patterns/Ex_Files_JavaScript_Patterns/Exercise Files/CH2/02_05/mysol_1/index.js
@@ -1,28 +1,27 @@
-let instance = null;
 
 class Car {
-    constructor(doors, engine, color) {
-        if (!instance) {
-            this.doors = doors;
-            this.engine = engine;
-            this.color = color;
-            instance = this;        
-        } else {
-            return instance;
-        }
-    }
+  constructor(doors, engine, color) {
+      this.doors = doors;
+      this.engine = engine;
+      this.color = color;   
+  }
 }
 
-// class SUV extends Car {
-//     constructor(doors, engine, color) {
-//         super(doors, engine, color);
-//         this.wheels = 4;
-//     }
-// }
+class carFactory {
+  createCar(type) {
+    switch(type) {
+      case 'civic':
+        return new Car(4, 'V6', 'grey');
+      case 'honda':
+        return new Car(2, "V8", 'red');
+    }
+  }
+}
 
 
-const civic = new Car(4, 'V6', 'grey');
-const honda = new Car(2, "V8", 'red');
+const factory = new carFactory();
+const myHonda = factory.createCar('honda');
+const myToyota = factory.createCar('toyota');
 
-console.log(civic);
-console.log(honda);
+console.log(myHonda);
+console.log(myToyota);
