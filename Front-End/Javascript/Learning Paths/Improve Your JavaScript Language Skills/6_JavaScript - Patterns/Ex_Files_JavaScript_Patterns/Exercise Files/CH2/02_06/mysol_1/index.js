de1f13872452811824f2ef8cef322fca722c0456
Jin Wang <specter01wj@gmail.com>
2023-01-23 17:50:54 -0500

    v11

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/6_JavaScript - Patterns/Ex_Files_JavaScript_Patterns/Exercise Files/CH2/02_06/mysol_1/index.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/6_JavaScript - Patterns/Ex_Files_JavaScript_Patterns/Exercise Files/CH2/02_06/mysol_1/index.js
@@ -1,23 +1,58 @@
+
 class Car {
-    constructor(doors, engine, color) {
-        this.doors = doors;
-        this.engine = engine;
-        this.color = color;   
-    }
+  constructor(doors, engine, color) {
+      this.doors = doors;
+      this.engine = engine;
+      this.color = color;   
+  }
 }
 
-class carFactory {
-    createCar(type) {
-        switch(type) {
-            case 'civic':
-                return new Car(4, 'V6', 'grey')
-            case 'honda':
-                return new Car(2, "V8", 'red')
-        }
+class CarFactory {
+  createCar(type) {
+    switch(type) {
+      case 'civic':
+        return new Car(4, 'V6', 'grey');
+      case 'honda':
+        return new Car(2, "V8", 'red');
     }
+  }
 }
 
-const factory = new carFactory();
-const myHonda = factory.createCar('honda');
+class SUV {
+  constructor(doors, engine, color) {
+      this.doors = doors;
+      this.engine = engine;
+      this.color = color;   
+  }
+}
 
-console.log(myHonda);
+class SuvFactory {
+  createCar(type) {
+      switch(type) {
+          case 'cx5':
+              return new Car(8, 'V8', 'yellow')
+          case 'sante fe':
+              return new Car(2, "V8", 'red')
+      }
+  }
+}
+
+const carFactory = new CarFactory();
+const suvFactory = new SuvFactory();
+
+const autoManufacturer = (type, model) => {
+  switch(type) {
+      case 'car':
+          return carFactory.createCar(model);
+      case 'suv':
+          return suvFactory.createCar(model);
+  }
+}
+
+
+const cx5 = autoManufacturer('suv', 'cx5');
+
+const civic = autoManufacturer('car', 'civic');
+
+console.log(cx5);
+console.log(civic);
