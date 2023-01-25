782f26fa45b96257f42f13f883be789f7751fee0
Jin Wang <specter01wj@gmail.com>
2023-01-25 11:54:08 -0500

    v5

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/6_JavaScript - Patterns/Ex_Files_JavaScript_Patterns/Exercise Files/CH4/04_05/mysol_1/index.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/6_JavaScript - Patterns/Ex_Files_JavaScript_Patterns/Exercise Files/CH4/04_05/mysol_1/index.js
@@ -1,62 +1,18 @@
-class Car {
-    constructor(doors, engine, color) {
-        this.doors = doors;
-        this.engine = engine;
-        this.color = color;   
-    }
+newsfeeds = [
+  {
+    type: 'top-headlines',
+    query: 'sources=bbc-news'
+  },
+  {
+    type: 'everything',
+    query: 'domains=techcrunch.com&language=en'
+  },
+  {
+    type: 'technology',
+    query: 'domains=comicbookmovie.com&language=en'
+  }
+]
+
+for (let feed of newsfeeds) {
+  console.log(feed.type);
 }
-
-class CarFactory {
-    createCar(type) {
-        switch(type) {
-            case 'civic':
-                return new Car(4, 'V6', 'grey')
-            case 'honda':
-                return new Car(2, "V8", 'red')
-        }
-    }
-}
-
-class SUV {
-    constructor(doors, engine, color) {
-        this.doors = doors;
-        this.engine = engine;
-        this.color = color;   
-    }
-}
-
-class SuvFactory {
-    createCar(type) {
-        switch(type) {
-            case 'cx5':
-                return new Car(4, 'V6', 'grey')
-            case 'sante fe':
-                return new Car(2, "V8", 'red')
-        }
-    }
-}
-
-let carMixin = {
-    revEngine() {
-        console.log(`The ${this.engine} engine is doing Vroom Vroom!`);
-        
-    }
-}
-
-const carFactory = new CarFactory();
-const suvFactory = new SuvFactory();
-
-const autoManufacturer = (type, model) => {
-    switch(type) {
-        case 'car':
-            return carFactory.createCar(model);
-        case 'suv':
-            return suvFactory.createCar(model);
-    }
-}
-
-Object.assign(Car.prototype, carMixin);
-
-const honda = autoManufacturer('car', 'honda');
-
-honda.revEngine();
