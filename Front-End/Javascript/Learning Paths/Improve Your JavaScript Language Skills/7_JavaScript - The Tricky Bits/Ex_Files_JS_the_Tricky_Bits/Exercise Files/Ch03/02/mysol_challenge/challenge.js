1e048ceb53fa118da3cd8eefbccd6b96746a4bb6
Jin Wang <specter01wj@gmail.com>
2023-01-28 23:07:31 -0500

    v21

--- Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/7_JavaScript - The Tricky Bits/Ex_Files_JS_the_Tricky_Bits/Exercise Files/Ch03/02/mysol_challenge/challenge.js
+++ Front-End/Javascript/Learning Paths/Improve Your JavaScript Language Skills/7_JavaScript - The Tricky Bits/Ex_Files_JS_the_Tricky_Bits/Exercise Files/Ch03/02/mysol_challenge/challenge.js
@@ -112,6 +112,7 @@
         randomInt: function (min, max) {
             min = Math.ceil(min);
 
+
             
             max = Math.floor(max);
             return Math.floor(Math.random() * (max - min) + min);
