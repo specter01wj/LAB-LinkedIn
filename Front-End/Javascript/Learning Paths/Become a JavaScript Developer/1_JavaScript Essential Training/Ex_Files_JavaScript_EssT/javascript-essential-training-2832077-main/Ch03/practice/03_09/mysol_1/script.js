cbfdf3b80ac5552d6ce0f92ead4c9986867cb678
Jin Wang <specter01wj@gmail.com>
2023-02-01 23:48:28 -0500

    v10

---
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch03/practice/03_09/mysol_1/script.js
@@ -0,0 +1,39 @@
+/**
+ * Practice: Making methods
+ *
+ * - Create a method for each object property.
+ * - The method receives a value to match the property to be changed.
+ * - Create a simple function to replace the current property value with the received value.
+ * - Test the method by sending new values and checking the properties in the console.
+ */
+
+const backpack = {
+  name: "Everyday Backpack",
+  volume: 30,
+  color: "grey",
+  pocketNum: 15,
+  strapLength: {
+    left: 26,
+    right: 26,
+  },
+  toggleLid: function (lidStatus) {
+    this.lidOpen = lidStatus;
+  },
+  newStrapLength: function (lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  },
+  changeAllVar: function(name, volume, color, pocketNum, left, right) {
+    this.name = name;
+    this.volume = volume;
+    this.color = color;
+    this.pocketNum = pocketNum;
+    this.strapLength.left = left;
+    this.strapLength.right = right;
+  }
+};
+
+
+console.log(backpack.changeAllVar('Civic', 45, 'red', 25, 100, 200));
+
+console.log(backpack.name);
