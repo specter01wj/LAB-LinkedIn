7e78fa9587bc89ba4136c461f0d7ec4b4c38ad3b
Jin Wang <specter01wj@gmail.com>
2023-02-03 00:30:38 -0500

    v3

--- Front-End/Javascript/Learning Paths/Become a JavaScript Developer/4_JavaScript - Scope/Ex_Files_JavaScript_Scope/Exercise Files/CH2/02_02/mysol_1/index.js
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/4_JavaScript - Scope/Ex_Files_JavaScript_Scope/Exercise Files/CH2/02_02/mysol_1/index.js
@@ -1,14 +1,16 @@
-// 'use strict';
+'use strict';
 
 const warrior = {
-  name: 'Jujin Take',
-  type: 'Ninja',
-  weapon: 'Shuriken',
-  agility: 79
-};
+    name: 'Jujin Take',
+    type: 'Ninja',
+    weapon: 'Shuriken',
+    agility: 79
+} // this is globally scoped
 
+// globally scoped 
 const screamWarrior = () => {
     let warrior2 = 'Samurai';
+    
     warrior3 = 'Viking';
     return {
         shootWarrior: () => {
@@ -23,5 +25,4 @@ const newWarrior = screamWarrior();
 newWarrior.shootWarrior();
 
 console.log(warrior, warrior3);
-
 
