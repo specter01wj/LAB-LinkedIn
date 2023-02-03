c71dd6b804b88d3935966676ba6ac08571911fca
Jin Wang <specter01wj@gmail.com>
2023-02-02 23:40:58 -0500

    v3

--- Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch04/04_02/mysol_1/script.js
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch04/04_02/mysol_1/script.js
@@ -23,5 +23,6 @@ document.body.innerHTML = content;
 console.log("The everydayPack object:", everydayPack);
 
 
+
 console.log("The pocketNum value:", everydayPack.pocketNum);
 console.log("Days since aquired:", everydayPack.backpackAge());
