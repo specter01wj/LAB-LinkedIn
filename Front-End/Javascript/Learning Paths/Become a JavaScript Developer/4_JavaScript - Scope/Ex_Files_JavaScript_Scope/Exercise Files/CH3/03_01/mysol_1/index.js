420a4b88789d71f7f616e4dbe880e690da372527
Jin Wang <specter01wj@gmail.com>
2023-02-03 12:01:21 -0500

    v1

---
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/4_JavaScript - Scope/Ex_Files_JavaScript_Scope/Exercise Files/CH3/03_01/mysol_1/index.js
@@ -0,0 +1,15 @@
+const screamWarrior = () => {
+  var warrior1 = 'Ninja';
+  let warrior2 = 'Samurai';
+
+  return `Our warriors are ${warrior1} and ${warrior2}`;
+}
+
+const screamWarrior2 = () => {
+  var warrior1 = 'Viking';
+  let warrior2 = 'Soldier';
+
+  return `Our warriors are ${warrior1} and ${warrior2}`;
+}
+
+console.log(screamWarrior2(), screamWarrior());
