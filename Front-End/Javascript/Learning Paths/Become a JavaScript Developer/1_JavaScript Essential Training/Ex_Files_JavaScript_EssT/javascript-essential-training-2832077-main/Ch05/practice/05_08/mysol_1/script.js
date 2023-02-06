1e87a204c8eb98c699ea7ddc993edd1d2dceab56
Jin Wang <specter01wj@gmail.com>
2023-02-06 01:22:50 -0500

    v13

--- Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch05/practice/05_08/mysol_1/script.js
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch05/practice/05_08/mysol_1/script.js
@@ -3,3 +3,8 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+const queryMaincontect = document.querySelector('.maincontent .page-header').classList.add('myclass');
+
+
+console.log(queryMaincontect);
