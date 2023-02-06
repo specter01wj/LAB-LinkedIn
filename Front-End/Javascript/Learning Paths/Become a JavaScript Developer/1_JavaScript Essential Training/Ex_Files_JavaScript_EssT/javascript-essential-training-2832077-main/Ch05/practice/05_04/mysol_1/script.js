bb67e1ce97deea90a59c1a8fcea701ae1cca19e9
Jin Wang <specter01wj@gmail.com>
2023-02-06 00:38:25 -0500

    v6

--- Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch05/practice/05_04/mysol_1/script.js
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch05/practice/05_04/mysol_1/script.js
@@ -2,3 +2,11 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+const queryArticle = document.querySelectorAll('article');
+const queryBackpack = document.querySelectorAll('.backpack');
+
+const selectElem = document.getElementsByClassName('backpack__strap');
+
+console.log(queryArticle);
+console.log(queryBackpack);
+console.log(selectElem);
