9c0007618f3d3eb95759bbfa51c5e2805b7bbf1e
Jin Wang <specter01wj@gmail.com>
2023-02-06 01:49:14 -0500

    v20

--- Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch05/05_10/mysol_1/script.js
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch05/05_10/mysol_1/script.js
@@ -60,3 +60,21 @@ newArticle.setAttribute("id", "everyday");
 newArticle.innerHTML = content;
 
 main.append(newArticle);
+
+const navContent = `
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Backpacks</a></li>
+      <li><a href="#">Other things</a></li>
+      <li><a href="#">Contact</a></li>
+`;
+
+const mainNav = document.createElement("nav")
+mainNav.classList.add("main-navigation");
+const navList = document.createElement("ul")
+navList.innerHTML = navContent
+mainNav.append(navList)
+
+document.querySelector(".siteheader").append(mainNav)
+
+
