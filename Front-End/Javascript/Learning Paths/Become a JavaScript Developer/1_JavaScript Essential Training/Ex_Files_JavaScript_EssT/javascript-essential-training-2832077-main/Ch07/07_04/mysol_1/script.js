c719903ecc11d4aba62a67e9f05381a7e04f3824
Jin Wang <specter01wj@gmail.com>
2023-02-07 14:00:24 -0500

    v6

--- Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch07/07_04/mysol_1/script.js
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch07/07_04/mysol_1/script.js
@@ -20,3 +20,25 @@ const deskArray = [
 
 console.log("Original array:", deskArray);
 
+
+deskArray.pop();
+console.log("New array:", deskArray);
+
+
+deskArray.unshift(deskArray.pop());
+console.log("Last item is now first:", deskArray);
+
+
+// deskArray.sort((a, b) => b - a);
+deskArray.sort();
+console.log("Sorted array:", deskArray);
+
+
+const foundItem = deskArray.find((item) => item === "notebook");
+console.log("Found item:", foundItem);
+
+
+let remove = "notebook";
+deskArray.splice(deskArray.indexOf(remove), 1);
+console.log(`Array with "${remove}" removed:`, deskArray);
+
