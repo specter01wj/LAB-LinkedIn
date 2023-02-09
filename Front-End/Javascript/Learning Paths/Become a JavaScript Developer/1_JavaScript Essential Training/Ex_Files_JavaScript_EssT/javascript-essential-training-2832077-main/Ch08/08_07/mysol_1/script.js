d296af4e720881400abe276eb2672901f9c6b237
Jin Wang <specter01wj@gmail.com>
2023-02-09 02:05:12 -0500

    v12

--- Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch08/08_07/mysol_1/script.js
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch08/08_07/mysol_1/script.js
@@ -3,17 +3,24 @@
  * @link https://developer.mozilla.org/en-US/docs/Glossary/Function
  */
 
-const tipCalculator = () => {
-  let sum = 29.95;
-  let percentage = 18;
+const tipCalculator = (sum, percentage, currency, prefix) => {
   let tip = sum * (percentage / 100);
   let total = sum + tip;
-  console.log(`
-  Sum before tip: ${sum}
-  Tip percentage: ${percentage}%
-  Tip:            ${tip.toFixed(2)}
-  Total:          ${total.toFixed(2)}
-`);
+  if (prefix) {
+    console.log(`
+      Sum before tip: ${currency}${sum}
+      Tip percentage: ${percentage}%
+      Tip:            ${currency}${tip.toFixed(2)}
+      Total:          ${currency}${total.toFixed(2)}
+    `);
+  } else {
+    console.log(`
+      Sum before tip: ${sum}${currency}
+      Tip percentage: ${percentage}%
+      Tip:            ${tip.toFixed(2)}${currency}
+      Total:          ${total.toFixed(2)}${currency}
+    `);
+  }
 };
 
-tipCalculator();
+tipCalculator(29.95, 18, "kr", false);
