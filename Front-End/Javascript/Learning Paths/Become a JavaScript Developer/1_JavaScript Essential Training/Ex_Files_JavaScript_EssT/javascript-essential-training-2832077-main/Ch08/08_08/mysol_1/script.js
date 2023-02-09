62a30f921d06d53bd0a71cd0c7e8b0b60b0c681e
Jin Wang <specter01wj@gmail.com>
2023-02-09 14:18:23 -0500

    v5

--- Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch08/08_08/mysol_1/script.js
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch08/08_08/mysol_1/script.js
@@ -8,15 +8,24 @@
  * @link http://www.lingoes.net/en/translator/langcode.htm
  */
 
-const tipCalculator = (sum, percentage) => {
+const formatter = (locale = "en-US", currency = "USD", value) => {
+  let formattedValue = new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+  }).format(value);
+
+  return formattedValue;
+};
+
+const tipCalculator = (sum, percentage, locale, currency) => {
   let tip = sum * (percentage / 100);
   let total = sum + tip;
   console.log(`
-      Sum before tip: ${sum}
-      Tip percentage: ${percentage}%
-      Tip:            ${tip.toFixed(2)}
-      Total:          ${total.toFixed(2)}
-    `);
+    Sum before tip: ${formatter(locale, currency, sum)}
+    Tip percentage: ${percentage}%
+    Tip:            ${formatter(locale, currency, tip)}
+    Total:          ${formatter(locale, currency, total)}
+  `);
 };
 
-tipCalculator(29.95, 18);
+tipCalculator(29.95, 18, "de-DE", "EUR");
