926b84820f4a98f931aa6105eaa90436d4f8f994
Jin Wang <specter01wj@gmail.com>
2023-02-09 00:08:24 -0500

    v2

--- Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch08/08_02/mysol_1/script.js
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch08/08_02/mysol_1/script.js
@@ -19,9 +19,9 @@ console.log("Do some math:", doSomeMath(5, 6));
 console.log("Do more math:", doMoreMath(5, 6));
 
 // Immediately Invoked Function Expression (IIFE)
-// (function () {
-//   let a = 4;
-//   let b = 6;
-//   let c = doSomeMath(a, b);
-//   console.log(`The sum of a and b is: ${c}`);
-// })();
+(function () {
+  let a = 4;
+  let b = 6;
+  let c = doSomeMath(a, b);
+  console.log(`The sum of a and b is: ${c}`);
+})();
