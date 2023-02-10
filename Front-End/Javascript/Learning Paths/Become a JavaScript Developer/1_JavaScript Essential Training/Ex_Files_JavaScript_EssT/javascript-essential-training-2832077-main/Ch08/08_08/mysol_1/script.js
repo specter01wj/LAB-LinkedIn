105a5e7b3334d5fdc737934b3128cca63f3057e8
Jin Wang <specter01wj@gmail.com>
2023-02-09 23:08:10 -0500

    v2

--- Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch08/08_08/mysol_1/script.js
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch08/08_08/mysol_1/script.js
@@ -12,6 +12,7 @@ const formatter = (locale = "en-US", currency = "USD", value) => {
   let formattedValue = new Intl.NumberFormat(locale, {
     style: "currency",
     currency: currency,
+
     
   }).format(value);
 
