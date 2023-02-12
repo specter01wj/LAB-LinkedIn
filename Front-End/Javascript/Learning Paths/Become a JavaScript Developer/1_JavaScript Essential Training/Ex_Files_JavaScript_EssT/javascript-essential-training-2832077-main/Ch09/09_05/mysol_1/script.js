93856b6ec67c3caf3cead72eb2c190fb19c71f98
Jin Wang <specter01wj@gmail.com>
2023-02-11 21:57:17 -0500

    v8

--- Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch09/09_05/mysol_1/script.js
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/1_JavaScript Essential Training/Ex_Files_JavaScript_EssT/javascript-essential-training-2832077-main/Ch09/09_05/mysol_1/script.js
@@ -9,18 +9,18 @@ import backpackObjectArray from "./components/data.js";
  * Add event listener to the lid-toggle button.
  */
 const lidToggle = function () {
-   
+
   // Find the current backpack object in backpackObjectArray
-  let backpackObject = backpackObjectArray.find( ({ id }) => id === this.parentElement.id );
-  
+  let backpackObject = backpackObjectArray.find(({ id }) => id === this.parentElement.id);
+
   // Toggle lidOpen status
-  backpackObject.lidOpen == true 
-    ? backpackObject.lidOpen = false 
+  backpackObject.lidOpen == true
+    ? backpackObject.lidOpen = false
     : backpackObject.lidOpen = true;
 
   // Toggle button text
-  this.innerText == "Open lid" 
-    ? this.innerText = "Close lid" 
+  this.innerText == "Open lid"
+    ? this.innerText = "Close lid"
     : this.innerText = "Open lid";
 
   // Set visible property status text
@@ -46,25 +46,19 @@ const backpackList = backpackObjectArray.map((backpack) => {
     </figure>
     <h1 class="backpack__name">${backpack.name}</h1>
     <ul class="backpack__features">
-      <li class="feature backpack__volume">Volume:<span> ${
-        backpack.volume
-      }l</span></li>
-      <li class="feature backpack__color">Color:<span> ${
-        backpack.color
-      }</span></li>
+      <li class="feature backpack__volume">Volume:<span> ${backpack.volume
+    }l</span></li>
+      <li class="feature backpack__color">Color:<span> ${backpack.color
+    }</span></li>
       <li class="feature backpack__age">Age:<span> ${backpack.backpackAge()} days old</span></li>
-      <li class="feature backpack__pockets">Number of pockets:<span> ${
-        backpack.pocketNum
-      }</span></li>
-      <li class="feature backpack__strap">Left strap length:<span> ${
-        backpack.strapLength.left
-      } inches</span></li>
-      <li class="feature backpack__strap">Right strap length:<span> ${
-        backpack.strapLength.right
-      } inches</span></li>
-      <li class="feature backpack__lid">Lid status: <span>${
-        backpack.lidOpen ? "open" : "closed"
-      }</span></li>
+      <li class="feature backpack__pockets">Number of pockets:<span> ${backpack.pocketNum
+    }</span></li>
+      <li class="feature backpack__strap">Left strap length:<span> ${backpack.strapLength.left
+    } inches</span></li>
+      <li class="feature backpack__strap">Right strap length:<span> ${backpack.strapLength.right
+    } inches</span></li>
+      <li class="feature backpack__lid">Lid status: <span>${backpack.lidOpen ? "open" : "closed"
+    }</span></li>
     </ul>
     <button class="lid-toggle">Open lid</button>
   `;
