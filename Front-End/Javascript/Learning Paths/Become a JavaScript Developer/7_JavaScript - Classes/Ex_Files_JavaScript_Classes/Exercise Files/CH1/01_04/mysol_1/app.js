d0e5a81993858f61dae6e74edf1f60411b39d55b
Jin Wang <specter01wj@gmail.com>
2023-02-20 17:47:55 -0500

    v2

---
+++ Front-End/Javascript/Learning Paths/Become a JavaScript Developer/7_JavaScript - Classes/Ex_Files_JavaScript_Classes/Exercise Files/CH1/01_04/mysol_1/app.js
@@ -0,0 +1,22 @@
+class Car {
+    constructor(doors, engine, color) {
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
+
+    carStats() {
+        return `This car has ${this.doors} doors, a ${this.engine} and a beautiful ${this.color} color!`;
+    }
+}
+
+const cx5 = new Car(4, 'V6', 'Grey');
+
+console.log(cx5);
+console.log(cx5.carStats());
+
+function sayHi() {
+    return console.log('Hello this function can be called anywhere!');
+}
+
+sayHi();
